f0968c638c01bdd3126dbb864e54725e35761410
bernie-g <berniegandin@gmail.com>
2020-09-24 19:50:43 -0400

    Fixed model reloading again

--- src/main/java/software/bernie/example/block/tile/FertilizerTileEntity.java
+++ src/main/java/software/bernie/example/block/tile/FertilizerTileEntity.java
@@ -1,11 +1,11 @@
 package software.bernie.example.block.tile;
 
 import net.minecraft.tileentity.TileEntity;
+import software.bernie.example.registry.TileRegistry;
+import software.bernie.geckolib.core.IAnimatable;
 import software.bernie.geckolib.core.builder.AnimationBuilder;
 import software.bernie.geckolib.core.controller.AnimationController;
-import software.bernie.geckolib.core.IAnimatable;
 import software.bernie.geckolib.core.event.predicate.AnimationTestPredicate;
-import software.bernie.example.registry.TileRegistry;
 import software.bernie.geckolib.core.manager.AnimationManager;
 
 public class FertilizerTileEntity extends TileEntity implements IAnimatable
@@ -13,10 +13,17 @@ public class FertilizerTileEntity extends TileEntity implements IAnimatable
 	private final AnimationManager manager = new AnimationManager();
 	private final AnimationController controller = new AnimationController(this, "controller", 0, this::predicate);
 
-	private <E extends TileEntity & IAnimatable> boolean predicate(AnimationTestPredicate<E> eSpecialAnimationPredicate)
+	private <E extends TileEntity & IAnimatable> boolean predicate(AnimationTestPredicate<E> event)
 	{
 		controller.transitionLengthTicks = 0;
-		controller.setAnimation(new AnimationBuilder().addAnimation("fertilizer.animation.deploy", true).addAnimation("fertilizer.animation.idle", true));
+		if (event.getEntity().getWorld().isRaining())
+		{
+			controller.setAnimation(new AnimationBuilder().addAnimation("fertilizer.animation.deploy", true).addAnimation("fertilizer.animation.idle", true));
+		}
+		else
+		{
+			controller.setAnimation(new AnimationBuilder().addAnimation("Botarium.anim.deploy", true).addAnimation("Botarium.anim.idle", true));
+		}
 		return true;
 	}
 
