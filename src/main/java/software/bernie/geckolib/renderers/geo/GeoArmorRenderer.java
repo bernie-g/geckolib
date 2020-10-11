b1653e83741474cd9043a173696793d35edc2cf1
AzureDoom <jerry99666@gmail.com>
2020-10-11 00:03:22 -0400

    Fixes Arms, Chest and attacking animation

--- src/main/java/software/bernie/geckolib/renderers/geo/GeoArmorRenderer.java
+++ src/main/java/software/bernie/geckolib/renderers/geo/GeoArmorRenderer.java
@@ -94,23 +94,32 @@ public abstract class GeoArmorRenderer<T extends ArmorItem & IAnimatable> extend
 		this.fitToBiped();
 		stack.push();
 		stack.translate(0, 0.01f, 0);
+		IBone rightArmBone = this.modelProvider.getBone(this.rightArmBone);
+		IBone leftArmBone = this.modelProvider.getBone(this.leftArmBone);
+		if(this.swingProgress > 0.0F) 
+		{
+				rightArmBone.setScaleZ(1.25F);
+				rightArmBone.setScaleX(1.25F);
+				leftArmBone.setScaleZ(1.3F);
+				leftArmBone.setScaleX(1.05F);
+		}
 		if(isSneak)
 		{
 			IBone headBone = this.modelProvider.getBone(this.headBone);
 			IBone bodyBone = this.modelProvider.getBone(this.bodyBone);
-			IBone rightArmBone = this.modelProvider.getBone(this.rightArmBone);
-			IBone leftArmBone = this.modelProvider.getBone(this.leftArmBone);
 			IBone rightLegBone = this.modelProvider.getBone(this.rightLegBone);
 			IBone leftLegBone = this.modelProvider.getBone(this.leftLegBone);
 			IBone rightBootBone = this.modelProvider.getBone(this.rightBootBone);
 			IBone leftBootBone = this.modelProvider.getBone(this.leftBootBone);
 			try
 			{
-				headBone.setPositionY(headBone.getPositionY() - 5);
-				bodyBone.setPositionZ(bodyBone.getPositionX() - 1);
-				bodyBone.setPositionY(bodyBone.getPositionX() - 3);
+				headBone.setPositionY(headBone.getPositionY() - 5.35F);
+				bodyBone.setPositionZ(bodyBone.getPositionX() - 0.4F);
+				bodyBone.setPositionY(bodyBone.getPositionX() - 3.5F);
 				rightArmBone.setPositionY(bodyBone.getPositionX() - 3);
+				rightArmBone.setPositionX(bodyBone.getPositionX() + 0.35F);
 				leftArmBone.setPositionY(bodyBone.getPositionX() - 3);
+				leftArmBone.setPositionX(bodyBone.getPositionX() - 0.35F);
 				rightLegBone.setPositionZ(bodyBone.getPositionX() + 4);
 				leftLegBone.setPositionZ(bodyBone.getPositionX() + 4);
 				rightBootBone.setPositionZ(bodyBone.getPositionX() + 4);
