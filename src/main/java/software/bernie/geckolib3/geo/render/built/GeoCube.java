0a075dedcd41babca3c8f59f58336109d57bc041
bernie-g <berniegandin@gmail.com>
2020-11-18 12:51:24 -0500

    Fixed datagens and crash on missing cube side

--- src/main/java/software/bernie/geckolib3/geo/render/built/GeoCube.java
+++ src/main/java/software/bernie/geckolib3/geo/render/built/GeoCube.java
@@ -106,39 +106,43 @@ public class GeoCube {
         GeoQuad quadUp;
         GeoQuad quadDown;
 
-        if (!isBoxUV) {
-            FaceUv west = faces.getWest();
-            FaceUv east = faces.getEast();
-            FaceUv north = faces.getNorth();
-            FaceUv south = faces.getSouth();
-            FaceUv up = faces.getUp();
-            FaceUv down = faces.getDown();
-            //Pass in vertices starting from the top right corner, then going counter-clockwise
-            quadWest = new GeoQuad(new GeoVertex[]{P4, P3, P1, P2}, west.getUv(), west.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.WEST);
-            quadEast = new GeoQuad(new GeoVertex[]{P7, P8, P6, P5}, east.getUv(), east.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.EAST);
-            quadNorth = new GeoQuad(new GeoVertex[]{P3, P7, P5, P1}, north.getUv(), north.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.NORTH);
-            quadSouth = new GeoQuad(new GeoVertex[]{P8, P4, P2, P6}, south.getUv(), south.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.SOUTH);
-            quadUp = new GeoQuad(new GeoVertex[]{P4, P8, P7, P3}, up.getUv(), up.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.UP);
-            quadDown = new GeoQuad(new GeoVertex[]{P6, P2, P1, P5}, down.getUv(), down.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.DOWN);
-        } else {
-            double[] UV = cubeIn.getUv().boxUVCoords;
-            Vector3d UVSize = VectorUtils.fromArray(cubeIn.getSize());
-            UVSize = new Vector3d(Math.floor(UVSize.x), Math.floor(UVSize.y), Math.floor(UVSize.z));
+		if (!isBoxUV)
+		{
+			FaceUv west = faces.getWest();
+			FaceUv east = faces.getEast();
+			FaceUv north = faces.getNorth();
+			FaceUv south = faces.getSouth();
+			FaceUv up = faces.getUp();
+			FaceUv down = faces.getDown();
+			//Pass in vertices starting from the top right corner, then going counter-clockwise
+			quadWest = west == null ? null : new GeoQuad(new GeoVertex[]{P4, P3, P1, P2}, west.getUv(), west.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.WEST);
+			quadEast = east == null ? null : new GeoQuad(new GeoVertex[]{P7, P8, P6, P5}, east.getUv(), east.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.EAST);
+			quadNorth = north == null ? null : new GeoQuad(new GeoVertex[]{P3, P7, P5, P1}, north.getUv(), north.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.NORTH);
+			quadSouth = south == null ? null : new GeoQuad(new GeoVertex[]{P8, P4, P2, P6}, south.getUv(), south.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.SOUTH);
+			quadUp = up == null ? null : new GeoQuad(new GeoVertex[]{P4, P8, P7, P3}, up.getUv(), up.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.UP);
+			quadDown = down == null ? null : new GeoQuad(new GeoVertex[]{P6, P2, P1, P5}, down.getUv(), down.getUvSize(), textureWidth, textureHeight, cubeIn.getMirror(), Direction.DOWN);
+		}
+		else
+		{
+			double[] UV = cubeIn.getUv().boxUVCoords;
+			Vector3d UVSize = VectorUtils.fromArray(cubeIn.getSize());
+			UVSize = new Vector3d(Math.floor(UVSize.x), Math.floor(UVSize.y), Math.floor(UVSize.z));
 
-            quadWest = new GeoQuad(new GeoVertex[]{P4, P3, P1, P2},  new double[] {UV[0] + UVSize.z + UVSize.x, UV[1] + UVSize.z}, new double[] {UVSize.z, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.WEST);
-            quadEast = new GeoQuad(new GeoVertex[]{P7, P8, P6, P5}, new double[] {UV[0], UV[1] + UVSize.z}, new double[] {UVSize.z, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.EAST);
-            quadNorth = new GeoQuad(new GeoVertex[]{P3, P7, P5, P1}, new double[] {UV[0] + UVSize.z, UV[1] + UVSize.z}, new double[] {UVSize.x, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.NORTH);
-            quadSouth = new GeoQuad(new GeoVertex[]{P8, P4, P2, P6},  new double[] {UV[0] + UVSize.z + UVSize.x + UVSize.z, UV[1] + UVSize.z}, new double[] {UVSize.x, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.SOUTH);
-            quadUp = new GeoQuad(new GeoVertex[]{P4, P8, P7, P3}, new double[] {UV[0] + UVSize.z, UV[1]}, new double[] {UVSize.x, UVSize.z}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.UP);
-            quadDown = new GeoQuad(new GeoVertex[]{P6, P2, P1, P5}, new double[] {UV[0] + UVSize.z + UVSize.x, UV[1]}, new double[] {UVSize.x, UVSize.z}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.DOWN);
-        }
+			quadWest = new GeoQuad(new GeoVertex[]{P4, P3, P1, P2},  new double[] {UV[0] + UVSize.z + UVSize.x, UV[1] + UVSize.z}, new double[] {UVSize.z, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.WEST);
+			quadEast = new GeoQuad(new GeoVertex[]{P7, P8, P6, P5}, new double[] {UV[0], UV[1] + UVSize.z}, new double[] {UVSize.z, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.EAST);
+			quadNorth = new GeoQuad(new GeoVertex[]{P3, P7, P5, P1}, new double[] {UV[0] + UVSize.z, UV[1] + UVSize.z}, new double[] {UVSize.x, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.NORTH);
+			quadSouth = new GeoQuad(new GeoVertex[]{P8, P4, P2, P6},  new double[] {UV[0] + UVSize.z + UVSize.x + UVSize.z, UV[1] + UVSize.z}, new double[] {UVSize.x, UVSize.y}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.SOUTH);
+			quadUp = new GeoQuad(new GeoVertex[]{P4, P8, P7, P3}, new double[] {UV[0] + UVSize.z, UV[1]}, new double[] {UVSize.x, UVSize.z}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.UP);
+			quadDown = new GeoQuad(new GeoVertex[]{P6, P2, P1, P5}, new double[] {UV[0] + UVSize.z + UVSize.x, UV[1]}, new double[] {UVSize.x, UVSize.z}, textureWidth, textureHeight, cubeIn.getMirror(), Direction.DOWN);
+		}
 
-        cube.quads[0] = quadWest;
-        cube.quads[1] = quadEast;
-        cube.quads[2] = quadNorth;
-        cube.quads[3] = quadSouth;
-        cube.quads[4] = quadUp;
-        cube.quads[5] = quadDown;
-        return cube;
-    }
+
+		cube.quads[0] = quadWest;
+		cube.quads[1] = quadEast;
+		cube.quads[2] = quadNorth;
+		cube.quads[3] = quadSouth;
+		cube.quads[4] = quadUp;
+		cube.quads[5] = quadDown;
+		return cube;
+	}
 }
--- src/main/java/software/bernie/geckolib3/resource/ResourceListener.java
+++ src/main/java/software/bernie/geckolib3/resource/ResourceListener.java
@@ -1,14 +1,25 @@
 package software.bernie.geckolib3.resource;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.ReloadableResourceManagerImpl;
+import net.minecraft.resource.ReloadableResourceManager;
+import software.bernie.geckolib3.GeckoLib;
 
-public class ResourceListener {
-    public static void registerReloadListener() {
-        if (MinecraftClient.getInstance().getResourceManager() == null) {
-            throw new RuntimeException("GeckoLib was initialized too early! If you are on fabric, please read the wiki on when to initialize!");
-        }
-        ReloadableResourceManagerImpl reloadable = (ReloadableResourceManagerImpl) MinecraftClient.getInstance().getResourceManager();
-        reloadable.registerListener(GeckoLibCache.getInstance()::resourceReload);
-    }
+public class ResourceListener
+{
+	public static void registerReloadListener()
+	{
+		if (MinecraftClient.getInstance() != null)
+		{
+			if (MinecraftClient.getInstance().getResourceManager() == null)
+			{
+				throw new RuntimeException("GeckoLib was initialized too early! If you are on fabric, please read the wiki on when to initialize!");
+			}
+			ReloadableResourceManager reloadable = (ReloadableResourceManager) MinecraftClient.getInstance().getResourceManager();
+			reloadable.registerListener(GeckoLibCache.getInstance()::resourceReload);
+		}
+		else
+		{
+			GeckoLib.LOGGER.warn("Minecraft.getInstance() was null, could not register reload listeners. Ignore if datagenning.");
+		}
+	}
 }
