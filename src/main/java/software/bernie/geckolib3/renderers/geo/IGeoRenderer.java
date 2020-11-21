450f0e8584dd2f0ae4d36b558b5de5e2e1071ede
McHorse <mchorselessrider@gmail.com>
2020-11-21 15:56:08 +0000

    Fix dark shades on 0 sized cubes (and matrix stack rendering)

--- src/main/java/software/bernie/geckolib3/renderers/geo/IGeoRenderer.java
+++ src/main/java/software/bernie/geckolib3/renderers/geo/IGeoRenderer.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.Vec3i;
 import org.lwjgl.opengl.GL11;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.geo.render.built.GeoCube;
@@ -13,13 +12,16 @@ import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.geo.render.built.GeoQuad;
 import software.bernie.geckolib3.geo.render.built.GeoVertex;
 import software.bernie.geckolib3.model.provider.GeoModelProvider;
-import software.bernie.geckolib3.util.RenderUtils;
+import software.bernie.geckolib3.util.MatrixStack;
 
+import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
 import java.awt.*;
 
 public interface IGeoRenderer<T>
 {
+	public static MatrixStack MATRIX_STACK = new MatrixStack();
+
 	default void render(GeoModel model, T animatable, float partialTicks, float red, float green, float blue, float alpha)
 	{
 		GlStateManager.disableCull();
@@ -27,80 +29,86 @@ public interface IGeoRenderer<T>
 		renderEarly(animatable, partialTicks, red, green, blue, alpha);
 
 		renderLate(animatable, partialTicks, red, green, blue, alpha);
+
+		BufferBuilder builder = Tessellator.getInstance().getBuffer();
+
+		builder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+
 		//Render all top level bones
 		for (GeoBone group : model.topLevelBones)
 		{
-			renderRecursively(group, red, green, blue, alpha);
+			renderRecursively(builder, group, red, green, blue, alpha);
 		}
+
+		Tessellator.getInstance().draw();
 
 		renderAfter(animatable, partialTicks, red, green, blue, alpha);
 		GlStateManager.disableRescaleNormal();
 		GlStateManager.enableCull();
 	}
 
-	default void renderRecursively(GeoBone bone, float red, float green, float blue, float alpha)
+	default void renderRecursively(BufferBuilder builder, GeoBone bone, float red, float green, float blue, float alpha)
 	{
-		GlStateManager.pushMatrix();
-		RenderUtils.translate(bone);
-		RenderUtils.moveToPivot(bone);
-		RenderUtils.rotate(bone);
-		RenderUtils.scale(bone);
-		RenderUtils.moveBackFromPivot(bone);
+		MATRIX_STACK.push();
+
+		MATRIX_STACK.translate(bone);
+		MATRIX_STACK.moveToPivot(bone);
+		MATRIX_STACK.rotate(bone);
+		MATRIX_STACK.scale(bone);
+		MATRIX_STACK.moveBackFromPivot(bone);
 
 		if (!bone.isHidden)
 		{
 			for (GeoCube cube : bone.childCubes)
 			{
 				GlStateManager.pushMatrix();
-				renderCube(cube, red, green, blue, alpha);
+				renderCube(builder, cube, red, green, blue, alpha);
 				GlStateManager.popMatrix();
 			}
 			for (GeoBone childBone : bone.childBones)
 			{
-				renderRecursively(childBone, red, green, blue, alpha);
+				renderRecursively(builder, childBone, red, green, blue, alpha);
 			}
 		}
 
-
-		GlStateManager.popMatrix();
+		MATRIX_STACK.pop();
 	}
 
-	default void renderCube(GeoCube cube, float red, float green, float blue, float alpha)
+	default void renderCube(BufferBuilder builder, GeoCube cube, float red, float green, float blue, float alpha)
 	{
-		BufferBuilder builder = Tessellator.getInstance().getBuffer();
-
-		RenderUtils.moveToPivot(cube);
-		RenderUtils.rotate(cube);
-		RenderUtils.moveBackFromPivot(cube);
-
-		builder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+		MATRIX_STACK.moveToPivot(cube);
+		MATRIX_STACK.rotate(cube);
+		MATRIX_STACK.moveBackFromPivot(cube);
 
 		for (GeoQuad quad : cube.quads)
 		{
-			Vec3i normal = quad.normal;
+			Vector3f normal = new Vector3f(quad.normal.getX(), quad.normal.getY(), quad.normal.getZ());
 
-			/* if (normal.getX() < 0)
+			MATRIX_STACK.getNormalMatrix().transform(normal);
+
+			/* Fix shading dark shading for flat cubes + compatibility wish Optifine shaders */
+			if ((cube.size.y == 0 || cube.size.z == 0) && normal.getX() < 0)
 			{
-				normal = new Vec3i(-normal.getX(), normal.getY(), normal.getZ());
+				normal.x *= -1;
 			}
-			if (normal.getY() < 0)
+			if ((cube.size.x == 0 || cube.size.z == 0) && normal.getY() < 0)
 			{
-				normal = new Vec3i(normal.getX(), -normal.getY(), normal.getZ());
+				normal.y *= -1;
 			}
-			if (normal.getZ() < 0)
+			if ((cube.size.x == 0 || cube.size.y == 0) && normal.getZ() < 0)
 			{
-				normal = new Vec3i(normal.getX(), normal.getY(), -normal.getZ());
-			} */
+				normal.z *= -1;
+			}
 
 			for (GeoVertex vertex : quad.vertices)
 			{
 				Vector4f vector4f = new Vector4f(vertex.position.getX(), vertex.position.getY(), vertex.position.getZ(), 1.0F);
 
+				MATRIX_STACK.getModelMatrix().transform(vector4f);
+
 				builder.pos(vector4f.getX(), vector4f.getY(), vector4f.getZ()).tex(vertex.textureU, vertex.textureV).color(red, green, blue, alpha).normal(normal.getX(), normal.getY(), normal.getZ()).endVertex();
 			}
 		}
-
-		Tessellator.getInstance().draw();
 	}
 
 	GeoModelProvider getGeoModelProvider();
---
+++ src/main/java/software/bernie/geckolib3/util/MatrixStack.java
@@ -0,0 +1,236 @@
+package software.bernie.geckolib3.util;
+
+import org.lwjgl.util.vector.Quaternion;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
+import software.bernie.geckolib3.geo.render.built.GeoCube;
+
+import javax.vecmath.Matrix3f;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Vector3f;
+import java.util.Stack;
+
+/**
+ * Simple implementation of a matrix stack
+ */
+public class MatrixStack
+{
+	private Stack<Matrix4f> model = new Stack<Matrix4f>();
+	private Stack<Matrix3f> normal = new Stack<Matrix3f>();
+
+	private Matrix4f tempModelMatrix = new Matrix4f();
+	private Matrix3f tempNormalMatrix = new Matrix3f();
+	private float[] tempArray = new float[16];
+
+	public MatrixStack()
+	{
+		Matrix4f model = new Matrix4f();
+		Matrix3f normal = new Matrix3f();
+
+		model.setIdentity();
+		normal.setIdentity();
+
+		this.model.add(model);
+		this.normal.add(normal);
+	}
+
+	public Matrix4f getModelMatrix()
+	{
+		return this.model.peek();
+	}
+
+	public Matrix3f getNormalMatrix()
+	{
+		return this.normal.peek();
+	}
+
+	public void push()
+	{
+		this.model.add(new Matrix4f(this.model.peek()));
+		this.normal.add(new Matrix3f(this.normal.peek()));
+	}
+
+	public void pop()
+	{
+		if (this.model.size() == 1)
+		{
+			throw new IllegalStateException("A one level stack can't be popped!");
+		}
+
+		this.model.pop();
+		this.normal.pop();
+	}
+
+	/* Translate */
+
+	public void translate(float x, float y, float z)
+	{
+		this.translate(new Vector3f(x, y, z));
+	}
+
+	public void translate(Vector3f vec)
+	{
+		this.tempModelMatrix.setIdentity();
+		this.tempModelMatrix.setTranslation(vec);
+
+		this.model.peek().mul(this.tempModelMatrix);
+	}
+
+	public void moveToPivot(GeoCube cube)
+	{
+		Vector3f pivot = cube.pivot;
+		this.translate(pivot.getX() / 16, pivot.getY() / 16, pivot.getZ() / 16);
+	}
+
+	public void moveBackFromPivot(GeoCube cube)
+	{
+		Vector3f pivot = cube.pivot;
+		this.translate(-pivot.getX() / 16, -pivot.getY() / 16, -pivot.getZ() / 16);
+	}
+
+	public void moveToPivot(GeoBone bone)
+	{
+		this.translate(bone.rotationPointX / 16, bone.rotationPointY / 16, bone.rotationPointZ / 16);
+	}
+
+	public void moveBackFromPivot(GeoBone bone)
+	{
+		this.translate(-bone.rotationPointX / 16, -bone.rotationPointY / 16, -bone.rotationPointZ / 16);
+	}
+
+	public void translate(GeoBone bone)
+	{
+		this.translate(-bone.getPositionX() / 16, bone.getPositionY() / 16, bone.getPositionZ() / 16);
+	}
+
+	/* Scale */
+
+	public void scale(float x, float y, float z)
+	{
+		this.tempModelMatrix.setIdentity();
+		this.tempModelMatrix.setM00(x);
+		this.tempModelMatrix.setM11(y);
+		this.tempModelMatrix.setM22(z);
+
+		this.model.peek().mul(this.tempModelMatrix);
+	}
+
+	public void scale(GeoBone bone)
+	{
+		this.scale(bone.getScaleX(), bone.getScaleY(), bone.getScaleZ());
+	}
+
+	/* Rotate */
+
+	public void rotateX(float radian)
+	{
+		this.tempModelMatrix.setIdentity();
+		this.tempModelMatrix.rotX(radian);
+
+		this.tempNormalMatrix.setIdentity();
+		this.tempNormalMatrix.rotX(radian);
+
+		this.model.peek().mul(this.tempModelMatrix);
+		this.normal.peek().mul(this.tempNormalMatrix);
+	}
+
+	public void rotateY(float radian)
+	{
+		this.tempModelMatrix.setIdentity();
+		this.tempModelMatrix.rotY(radian);
+
+		this.tempNormalMatrix.setIdentity();
+		this.tempNormalMatrix.rotY(radian);
+
+		this.model.peek().mul(this.tempModelMatrix);
+		this.normal.peek().mul(this.tempNormalMatrix);
+	}
+
+	public void rotateZ(float radian)
+	{
+		this.tempModelMatrix.setIdentity();
+		this.tempModelMatrix.rotZ(radian);
+
+		this.tempNormalMatrix.setIdentity();
+		this.tempNormalMatrix.rotZ(radian);
+
+		this.model.peek().mul(this.tempModelMatrix);
+		this.normal.peek().mul(this.tempNormalMatrix);
+	}
+
+	public void rotate(GeoBone bone)
+	{
+		if (bone.getRotationZ() != 0.0F)
+		{
+			this.rotateZ(bone.getRotationZ());
+		}
+
+		if (bone.getRotationY() != 0.0F)
+		{
+			this.rotateY(bone.getRotationY());
+		}
+
+		if (bone.getRotationX() != 0.0F)
+		{
+			this.rotateX(bone.getRotationX());
+		}
+	}
+
+	public void rotate(GeoCube bone)
+	{
+		Vector3f rotation = bone.rotation;
+		Quaternion quat = new Quaternion(rotation.getX(), rotation.getY(), rotation.getZ(), 0);
+
+		this.tempNormalMatrix.setIdentity();
+		this.tempModelMatrix.setIdentity();
+
+		/* https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/index.htm */
+		float xx = quat.x * quat.x;
+		float xy = quat.x * quat.y;
+		float xz = quat.x * quat.z;
+		float xw = quat.x * quat.w;
+		float yy = quat.y * quat.y;
+		float yz = quat.y * quat.z;
+		float yw = quat.y * quat.w;
+		float zz = quat.z * quat.z;
+		float zw = quat.z * quat.w;
+
+		this.tempArray[0] = 1.0F - 2.0F * (yy + zz);
+		this.tempArray[1] = 2.0F * (xy + zw);
+		this.tempArray[2] = 2.0F * (xz - yw);
+		this.tempArray[3] = 0.0F;
+
+		this.tempArray[4] = 2.0F * (xy - zw);
+		this.tempArray[5] = 1.0F - 2.0F * (xx + zz);
+		this.tempArray[6] = 2.0F * (yz + xw);
+		this.tempArray[7] = 0.0F;
+
+		this.tempArray[8] = 2.0F * (xz + yw);
+		this.tempArray[9] = 2.0F * (yz - xw);
+		this.tempArray[10] = 1.0F - 2.0F * (xx + yy);
+		this.tempArray[11] = 0.0F;
+
+		this.tempArray[12] = 0.0F;
+		this.tempArray[13] = 0.0F;
+		this.tempArray[14] = 0.0F;
+		this.tempArray[15] = 1.0F;
+
+		this.tempModelMatrix.set(this.tempArray);
+
+		this.tempArray[0] = 1.0F - 2.0F * (yy + zz);
+		this.tempArray[1] = 2.0F * (xy + zw);
+		this.tempArray[2] = 2.0F * (xz - yw);
+
+		this.tempArray[3] = 2.0F * (xy - zw);
+		this.tempArray[4] = 1.0F - 2.0F * (xx + zz);
+		this.tempArray[5] = 2.0F * (yz + xw);
+
+		this.tempArray[6] = 2.0F * (xz + yw);
+		this.tempArray[7] = 2.0F * (yz - xw);
+		this.tempArray[8] = 1.0F - 2.0F * (xx + yy);
+
+		this.tempNormalMatrix.set(this.tempArray);
+
+		this.model.peek().mul(this.tempModelMatrix);
+		this.normal.peek().mul(this.tempNormalMatrix);
+	}
+}
