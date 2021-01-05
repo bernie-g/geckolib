64b5edf0b35fcb28eb2346750212e51fd1f9c4a9
AzureDoom <jerry99666@gmail.com>
2021-01-04 20:36:30 -0500

    Update GeoEntityRenderer.java

--- src/main/java/software/bernie/geckolib3/renderers/geo/GeoEntityRenderer.java
+++ src/main/java/software/bernie/geckolib3/renderers/geo/GeoEntityRenderer.java
@@ -3,6 +3,7 @@ package software.bernie.geckolib3.renderers.geo;
 import com.google.common.collect.Lists;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Vector3f;
