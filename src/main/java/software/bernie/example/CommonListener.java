9264e2969658273eea01a76dd67f35ae9fda55e9
Paspartout <Paspartout@users.noreply.github.com>
2021-01-22 20:41:26 +0100

    Make example entities spawnable in 1.16 by adding attributes
    This should be updated along with mappings/forge at some point.

---
+++ src/main/java/software/bernie/example/CommonListener.java
@@ -0,0 +1,25 @@
+package software.bernie.example;
+
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import software.bernie.example.registry.EntityRegistry;
+import software.bernie.geckolib3.GeckoLib;
+
+@Mod.EventBusSubscriber(modid = GeckoLib.ModID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CommonListener {
+    @SubscribeEvent
+    public static void registerEntityAttributes(FMLCommonSetupEvent event) {
+        if (!FMLEnvironment.production && !GeckoLibMod.DISABLE_IN_DEV) {
+            event.enqueueWork(() -> {
+                // TODO: Update these with new mappings
+                GlobalEntityTypeAttributes.put(EntityRegistry.BIKE_ENTITY.get(), MobEntity.func_233666_p_().func_233815_a_(Attributes.field_233818_a_, 1.0D).func_233813_a_());
+                GlobalEntityTypeAttributes.put(EntityRegistry.GEO_EXAMPLE_ENTITY.get(), MobEntity.func_233666_p_().func_233815_a_(Attributes.field_233818_a_, 1.0D).func_233813_a_());
+            });
+        }
+    }
+}
