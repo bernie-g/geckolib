7b8d955063dc28542e491ad5bb1db99e4950fd14
AzureZhen <rcraft@cyberconnects2.com>
2021-02-01 16:47:19 -0500

    v3.0.17

--- src/main/java/software/bernie/geckolib3/resource/GeckoLibCache.java
+++ src/main/java/software/bernie/geckolib3/resource/GeckoLibCache.java
@@ -1,6 +1,5 @@
 package software.bernie.geckolib3.resource;
 
-import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
@@ -21,70 +20,69 @@ import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.molang.MolangRegistrar;
 
 public class GeckoLibCache {
-    private HashMap<Identifier, AnimationFile> animations = new HashMap<>();
-    private HashMap<Identifier, GeoModel> geoModels = new HashMap<>();
-    private static GeckoLibCache INSTANCE;
-    public final MolangParser parser = new MolangParser();
-    private final AnimationFileLoader animationLoader;
-    private final GeoModelLoader modelLoader;
+	private ConcurrentHashMap<Identifier, AnimationFile> animations = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<Identifier, GeoModel> geoModels = new ConcurrentHashMap<>();
+	private static GeckoLibCache INSTANCE;
+	public final MolangParser parser = new MolangParser();
+	private final AnimationFileLoader animationLoader;
+	private final GeoModelLoader modelLoader;
 
-    public HashMap<Identifier, AnimationFile> getAnimations()
-    {
-        if(!GeckoLib.hasInitialized)
-        {
-            throw new RuntimeException("GeckoLib was never initialized! Please read the documentation!");
-        }
-        return animations;
-    }
+	public ConcurrentHashMap<Identifier, AnimationFile> getAnimations() {
+		if (!GeckoLib.hasInitialized) {
+			throw new RuntimeException("GeckoLib was never initialized! Please read the documentation!");
+		}
+		return animations;
+	}
 
-    public HashMap<Identifier, GeoModel> getGeoModels()
-    {
-        if(!GeckoLib.hasInitialized)
-        {
-            throw new RuntimeException("GeckoLib was never initialized! Please read the documentation!");
-        }
-        return geoModels;
-    }
+	public ConcurrentHashMap<Identifier, GeoModel> getGeoModels() {
+		if (!GeckoLib.hasInitialized) {
+			throw new RuntimeException("GeckoLib was never initialized! Please read the documentation!");
+		}
+		return geoModels;
+	}
 
-    protected GeckoLibCache() {
-        this.animationLoader = new AnimationFileLoader();
-        this.modelLoader = new GeoModelLoader();
-        MolangRegistrar.registerVars(parser);
-    }
+	protected GeckoLibCache() {
+		this.animationLoader = new AnimationFileLoader();
+		this.modelLoader = new GeoModelLoader();
+		MolangRegistrar.registerVars(parser);
+	}
 
+	public static GeckoLibCache getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new GeckoLibCache();
+			return INSTANCE;
+		}
+		return INSTANCE;
+	}
 
-    public static GeckoLibCache getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new GeckoLibCache();
-            return INSTANCE;
-        }
-        return INSTANCE;
-    }
+	public CompletableFuture<Void> resourceReload(ResourceReloadListener.Synchronizer stage,
+			ResourceManager resourceManager, Profiler preparationsProfiler, Profiler reloadProfiler,
+			Executor backgroundExecutor, Executor gameExecutor) {
+		ConcurrentHashMap<Identifier, AnimationFile> tempAnimations = new ConcurrentHashMap<>();
+		ConcurrentHashMap<Identifier, GeoModel> tempModels = new ConcurrentHashMap<>();
 
-    public CompletableFuture<Void> resourceReload(ResourceReloadListener.Synchronizer stage, ResourceManager resourceManager, Profiler preparationsProfiler, Profiler reloadProfiler, Executor backgroundExecutor, Executor gameExecutor) {
-    	ConcurrentHashMap<Identifier, AnimationFile> tempAnimations = new ConcurrentHashMap<>();
-    	ConcurrentHashMap<Identifier, GeoModel> tempModels = new ConcurrentHashMap<>();
+		CompletableFuture[] animationFileFutures = resourceManager
+				.findResources("animations", fileName -> fileName.endsWith(".json")).stream()
+				.map(location -> CompletableFuture.supplyAsync(() -> location))
+				.map(completable -> completable.thenAcceptAsync(resource -> tempAnimations.put(resource,
+						animationLoader.loadAllAnimations(parser, resource, resourceManager))))
+				.toArray(CompletableFuture[]::new);
 
-        CompletableFuture[] animationFileFutures = resourceManager.findResources("animations", fileName -> fileName.endsWith(".json"))
-                .stream()
-                .map(location -> CompletableFuture.supplyAsync(() -> location))
-                .map(completable -> completable.thenAcceptAsync(resource -> tempAnimations.put(resource, animationLoader.loadAllAnimations(parser, resource, resourceManager))))
-                .toArray(CompletableFuture[]::new);
-
-        CompletableFuture[] geoModelFutures = resourceManager.findResources("geo", fileName -> fileName.endsWith(".json"))
-                .stream()
-                .map(location -> CompletableFuture.supplyAsync(() -> location))
-                .map(completable -> completable.thenAcceptAsync(resource -> tempModels.put(resource, modelLoader.loadModel(resourceManager, resource))))
-                .toArray(CompletableFuture[]::new);
-        return CompletableFuture.allOf(ArrayUtils.addAll(animationFileFutures, geoModelFutures)).thenAccept(x -> {
-        	//Retain our behavior of completely replacing the old model map on reload
-			HashMap<Identifier, AnimationFile> hashAnim = new HashMap<>();
+		CompletableFuture[] geoModelFutures = resourceManager
+				.findResources("geo", fileName -> fileName.endsWith(".json")).stream()
+				.map(location -> CompletableFuture.supplyAsync(() -> location))
+				.map(completable -> completable.thenAcceptAsync(
+						resource -> tempModels.put(resource, modelLoader.loadModel(resourceManager, resource))))
+				.toArray(CompletableFuture[]::new);
+		return CompletableFuture.allOf(ArrayUtils.addAll(animationFileFutures, geoModelFutures)).thenAccept(x -> {
+			// Retain our behavior of completely replacing the old model map on reload
+			ConcurrentHashMap<Identifier, AnimationFile> hashAnim = new ConcurrentHashMap<>();
 			hashAnim.putAll(tempAnimations);
 			animations = hashAnim;
 
-			HashMap<Identifier, GeoModel> hashModel = new HashMap<>();
+			ConcurrentHashMap<Identifier, GeoModel> hashModel = new ConcurrentHashMap<>();
 			hashModel.putAll(tempModels);
 			geoModels = hashModel;
-        }).thenCompose(stage::whenPrepared);
-    }
+		}).thenCompose(stage::whenPrepared);
+	}
 }
