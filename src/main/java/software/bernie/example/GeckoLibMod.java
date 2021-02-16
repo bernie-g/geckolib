e592651e6ce7b195f2f5fbd3ff6b415e60037326
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-02-16 03:28:41 +0000

    Fix creative tab in the example showing empty

--- src/main/java/software/bernie/example/GeckoLibMod.java
+++ src/main/java/software/bernie/example/GeckoLibMod.java
@@ -6,23 +6,14 @@
 package software.bernie.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
-import software.bernie.example.registry.BlockRegistry;
-import software.bernie.example.registry.EntityRegistry;
-import software.bernie.example.registry.ItemRegistry;
-import software.bernie.example.registry.SoundRegistry;
-import software.bernie.example.registry.TileRegistry;
+import software.bernie.example.registry.*;
 import software.bernie.geckolib3.GeckoLib;
 
 public class GeckoLibMod implements ModInitializer {
 	public static boolean DISABLE_IN_DEV = false;
 	boolean isDevelopmentEnvironment = FabricLoader.getInstance().isDevelopmentEnvironment();
-	public static ItemGroup geckolibItemGroup;
 
 	@Override
 	public void onInitialize() {
@@ -37,8 +28,6 @@ public class GeckoLibMod implements ModInitializer {
 			new TileRegistry();
 			new BlockRegistry();
 			new SoundRegistry();
-			geckolibItemGroup = FabricItemGroupBuilder.create(new Identifier(GeckoLib.ModID, "geckolib_examples"))
-					.icon(() -> new ItemStack(ItemRegistry.JACK_IN_THE_BOX)).build();
 		}
 	}
 }
