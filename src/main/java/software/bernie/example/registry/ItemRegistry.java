558e098a813c00a3e70fa9382732f01c66a4a6b2
João <12478030+YummyLoop@users.noreply.github.com>
2021-02-20 15:04:59 +0000

    Change deprecated gradle features + fix typo + fix empty creative tab in the example (#108)
    * Upgrade gradle, replace deprecated features
    
    * Fix project name typo
    
    * Revert gradle version, geckolib core needs to be updated?
    
    * Fix creative tab in the example showing empty

--- src/main/java/software/bernie/example/registry/ItemRegistry.java
+++ src/main/java/software/bernie/example/registry/ItemRegistry.java
@@ -1,28 +1,31 @@
 package software.bernie.example.registry;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorMaterials;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import software.bernie.example.GeckoLibMod;
+import net.minecraft.item.*;
+import net.minecraft.util.Identifier;
 import software.bernie.example.item.JackInTheBoxItem;
 import software.bernie.example.item.PotatoArmorItem;
+import software.bernie.geckolib3.GeckoLib;
 
 public class ItemRegistry {
-	public static final JackInTheBoxItem JACK_IN_THE_BOX = RegistryUtils.registerItem("jackintheboxitem",
-			new JackInTheBoxItem(new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
 
+	public static ItemGroup geckolibItemGroup = FabricItemGroupBuilder.create(new Identifier(GeckoLib.ModID, "geckolib_examples"))
+			.icon(() -> new ItemStack(ItemRegistry.JACK_IN_THE_BOX)).build();
+
+	public static final JackInTheBoxItem JACK_IN_THE_BOX = RegistryUtils.registerItem("jackintheboxitem",
+			new JackInTheBoxItem(new Item.Settings().group(geckolibItemGroup)));
 	public static final PotatoArmorItem POTATO_HEAD = RegistryUtils.registerItem("potato_head", new PotatoArmorItem(
-			ArmorMaterials.DIAMOND, EquipmentSlot.HEAD, new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+			ArmorMaterials.DIAMOND, EquipmentSlot.HEAD, new Item.Settings().group(geckolibItemGroup)));
 	public static final PotatoArmorItem POTATO_CHEST = RegistryUtils.registerItem("potato_chest", new PotatoArmorItem(
-			ArmorMaterials.DIAMOND, EquipmentSlot.CHEST, new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+			ArmorMaterials.DIAMOND, EquipmentSlot.CHEST, new Item.Settings().group(geckolibItemGroup)));
 	public static final PotatoArmorItem POTATO_LEGGINGS = RegistryUtils.registerItem("potato_leggings",
 			new PotatoArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.LEGS,
-					new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+					new Item.Settings().group(geckolibItemGroup)));
 	public static final PotatoArmorItem POTATO_BOOTS = RegistryUtils.registerItem("potato_boots", new PotatoArmorItem(
-			ArmorMaterials.DIAMOND, EquipmentSlot.FEET, new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+			ArmorMaterials.DIAMOND, EquipmentSlot.FEET, new Item.Settings().group(geckolibItemGroup)));
 	public static final BlockItem BOTARIUM = RegistryUtils.registerItem("botarium",
-			new BlockItem(BlockRegistry.BOTARIUM_BLOCK, new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+			new BlockItem(BlockRegistry.BOTARIUM_BLOCK, new Item.Settings().group(geckolibItemGroup)));
 	public static final BlockItem FERTILIZER = RegistryUtils.registerItem("fertilizer",
-			new BlockItem(BlockRegistry.FERTILIZER_BLOCK, new Item.Settings().group(GeckoLibMod.geckolibItemGroup)));
+			new BlockItem(BlockRegistry.FERTILIZER_BLOCK, new Item.Settings().group(geckolibItemGroup)));
 }
