743427be0053c46990bc17a47857ac5334fff9c9
AzureZhen <rcraft@cyberconnects2.com>
2021-06-25 03:53:15 -0400

    v3.0.6

--- src/main/java/software/bernie/geckolib3/compat/PatchouliCompat.java
+++ src/main/java/software/bernie/geckolib3/compat/PatchouliCompat.java
@@ -1,12 +1,19 @@
 package software.bernie.geckolib3.compat;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
+import vazkii.patchouli.client.book.gui.GuiBook;
 
 public class PatchouliCompat {
 
+	@SuppressWarnings("resource")
 	public static void patchouliLoaded(MatrixStack matrixStackIn) {
 //	    Patchouli doesnt exist on fabric 1.17 snapshots :p
-/*		Class<GuiBook> patchouli = GuiBook.class;
+		Class<GuiBook> patchouli = GuiBook.class;
 		boolean screen = MinecraftClient.getInstance().inGameHud.equals(patchouli);
 		if (screen) {
 			matrixStackIn.push();
@@ -17,6 +24,6 @@ public class PatchouliCompat {
 			RenderSystem.enableDepthTest();
 			DiffuseLighting.enableGuiDepthLighting();
 			matrixStackIn.pop();
-		}*/
+		}
 	}
 }
