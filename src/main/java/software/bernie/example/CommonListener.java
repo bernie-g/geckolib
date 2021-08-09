fa28c77a3ee7775d5227163bbf7def19c564718a
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-08-09 18:52:08 +0200

    Use FMLEnvironment instead of FMLLoader to determine whether running in production

--- src/main/java/software/bernie/example/CommonListener.java
+++ src/main/java/software/bernie/example/CommonListener.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import software.bernie.example.registry.EntityRegistry;
 import software.bernie.geckolib3.GeckoLib;
 
@@ -13,7 +13,7 @@ import software.bernie.geckolib3.GeckoLib;
 public class CommonListener {
 	@SubscribeEvent
 	public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
-		if (!FMLLoader.isProduction() && !GeckoLibMod.DISABLE_IN_DEV) {
+		if (!FMLEnvironment.production && !GeckoLibMod.DISABLE_IN_DEV) {
 			event.put(EntityRegistry.BIKE_ENTITY.get(),
 					MobEntity.createMobAttributes().add(Attributes.MAX_HEALTH, 1.0D).build());
 			event.put(EntityRegistry.GEO_EXAMPLE_ENTITY.get(),
