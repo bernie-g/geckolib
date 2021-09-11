e0cdd63d8039adc43dc4a84088017a7b0745f1f0
AzureZhen <rcraft@cyberconnects2.com>
2021-09-11 19:35:10 -0400

    removed unneeded aw, using the proper method now

--- src/main/java/software/bernie/example/registry/TileRegistry.java
+++ src/main/java/software/bernie/example/registry/TileRegistry.java
@@ -1,13 +1,17 @@
 package software.bernie.example.registry;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.registry.Registry;
 import software.bernie.example.block.tile.BotariumTileEntity;
 import software.bernie.example.block.tile.FertilizerTileEntity;
+import software.bernie.geckolib3.GeckoLib;
 
 public class TileRegistry {
-	public static final BlockEntityType<BotariumTileEntity> BOTARIUM_TILE = RegistryUtils.registerBlockEntity(
-			"botariumtile", BlockEntityType.Builder.create(BotariumTileEntity::new, BlockRegistry.BOTARIUM_BLOCK));
-	public static final BlockEntityType<FertilizerTileEntity> FERTILIZER = RegistryUtils.registerBlockEntity(
-			"fertilizertile",
-			BlockEntityType.Builder.create(FertilizerTileEntity::new, BlockRegistry.FERTILIZER_BLOCK));
+	public static final BlockEntityType<BotariumTileEntity> BOTARIUM_TILE = Registry.register(
+			Registry.BLOCK_ENTITY_TYPE, GeckoLib.ModID + ":botariumtile",
+			FabricBlockEntityTypeBuilder.create(BotariumTileEntity::new, BlockRegistry.BOTARIUM_BLOCK).build(null));
+	public static final BlockEntityType<FertilizerTileEntity> FERTILIZER = Registry.register(Registry.BLOCK_ENTITY_TYPE,
+			GeckoLib.ModID + ":fertilizertile",
+			FabricBlockEntityTypeBuilder.create(FertilizerTileEntity::new, BlockRegistry.FERTILIZER_BLOCK).build(null));
 }
--- src/main/resources/geckolib.aw
+++ src/main/resources/geckolib.aw
@@ -1,4 +1,3 @@
 accessWidener	v1	named
-accessible class net/minecraft/block/entity/BlockEntityType$BlockEntityFactory
 accessible field net/minecraft/entity/Entity yaw F
 accessible field net/minecraft/entity/Entity pitch F
