1c92295d10834191dd595b2e5411b41df69d4b41
AzureZhen <rcraft@cyberconnects2.com>
2021-11-05 02:50:33 -0400

    v3.0.39

--- src/main/java/software/bernie/example/entity/GeoExampleEntity.java
+++ src/main/java/software/bernie/example/entity/GeoExampleEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.IAnimationTickable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.controller.AnimationController;
@@ -13,7 +14,7 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class GeoExampleEntity extends CreatureEntity implements IAnimatable {
+public class GeoExampleEntity extends CreatureEntity implements IAnimatable, IAnimationTickable {
 	private AnimationFactory factory = new AnimationFactory(this);
 
 	private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
@@ -40,5 +41,10 @@ public class GeoExampleEntity extends CreatureEntity implements IAnimatable {
 	protected void registerGoals() {
 		this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 6.0F));
 		super.registerGoals();
+	}
+
+	@Override
+	public int tickTimer() {
+		return ticksExisted;
 	}
 }
