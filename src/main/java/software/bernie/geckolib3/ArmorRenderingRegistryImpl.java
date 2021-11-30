9fa7e7879998db85fb12b44dfe6410d595456305
AzureZhen <rcraft@cyberconnects2.com>
2021-11-30 16:51:45 -0500

    v3.0.21

--- src/main/java/software/bernie/geckolib3/ArmorRenderingRegistryImpl.java
+++ src/main/java/software/bernie/geckolib3/ArmorRenderingRegistryImpl.java
@@ -14,34 +14,11 @@ public final class ArmorRenderingRegistryImpl {
 
 	public static BipedEntityModel<LivingEntity> getArmorModel(LivingEntity entity, ItemStack stack, EquipmentSlot slot,
 			BipedEntityModel<LivingEntity> defaultModel) {
-//		if (!stack.isEmpty()) {
-//			ArmorRenderingRegistry.ModelProvider provider = ((ArmorProviderExtensions) stack.getItem())
-//					.fabric_getArmorModelProvider();
-//			if (provider != null) {
-//				return provider.getArmorModel(entity, stack, slot, defaultModel);
-//			}
-//			if (stack.getItem() instanceof ArmorProvider) {
-//				return ((ArmorProvider) stack.getItem()).getArmorModel(entity, stack, slot, defaultModel);
-//			}
-//		}
-
 		return defaultModel;
 	}
 
 	public static Identifier getArmorTexture(LivingEntity entity, ItemStack stack, EquipmentSlot slot,
 			boolean secondLayer, @Nullable String suffix, Identifier defaultTexture) {
-//		if (!stack.isEmpty()) {
-//			ArmorRenderingRegistry.TextureProvider provider = ((ArmorProviderExtensions) stack.getItem())
-//					.fabric_getArmorTextureProvider();
-//
-//			if (provider != null) {
-//				return provider.getArmorTexture(entity, stack, slot, secondLayer, suffix, defaultTexture);
-//			}
-//			if (stack.getItem() instanceof ArmorProvider) {
-//				return ((ArmorProvider) stack.getItem()).getArmorTexture(entity, stack, slot, defaultTexture);
-//			}
-//		}
-
 		return defaultTexture;
 	}
 }
