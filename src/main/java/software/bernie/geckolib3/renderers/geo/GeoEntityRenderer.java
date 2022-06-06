8a08086316aa7d94dd8fcf16bc7c1ac81fd3a9d4
AzureZhen <rcraft@cyberconnects2.com>
2022-06-05 22:30:40 -0400

    v3.0.26

--- src/main/java/software/bernie/geckolib3/renderers/geo/GeoEntityRenderer.java
+++ src/main/java/software/bernie/geckolib3/renderers/geo/GeoEntityRenderer.java
@@ -7,10 +7,15 @@ import com.eliotlash.mclib.utils.Interpolations;
 import com.google.common.collect.Lists;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityHanging;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EnumPlayerModelParts;
@@ -137,6 +142,12 @@ public abstract class GeoEntityRenderer<T extends EntityLivingBase & IAnimatable
 						renderColor);
 			}
 		}
+		if (entity instanceof EntityLiving) {
+			Entity leashHolder = ((EntityLiving) entity).getLeashHolder();
+			if (leashHolder != null) {
+				this.renderLeash((EntityLiving) entity, x, y, z, entityYaw, partialTicks);
+			}
+		}
 
 		if (flag) {
 			RenderHurtColor.unset();
@@ -250,4 +261,115 @@ public abstract class GeoEntityRenderer<T extends EntityLivingBase & IAnimatable
 		return RenderHurtColor.set(entityLivingBaseIn, partialTicks);
 	}
 
+	protected void renderLeash(EntityLiving entityLivingIn, double x, double y, double z, float entityYaw,
+			float partialTicks) {
+		Entity entity = entityLivingIn.getLeashHolder();
+
+		if (entity != null) {
+			y = y - (1.6D - (double) entityLivingIn.height) * 0.5D;
+			Tessellator tessellator = Tessellator.getInstance();
+			BufferBuilder bufferbuilder = tessellator.getBuffer();
+			double d0 = this.interpolateValue((double) entity.prevRotationYaw, (double) entity.rotationYaw,
+					(double) (partialTicks * 0.5F)) * 0.01745329238474369D;
+			double d1 = this.interpolateValue((double) entity.prevRotationPitch, (double) entity.rotationPitch,
+					(double) (partialTicks * 0.5F)) * 0.01745329238474369D;
+			double d2 = Math.cos(d0);
+			double d3 = Math.sin(d0);
+			double d4 = Math.sin(d1);
+
+			if (entity instanceof EntityHanging) {
+				d2 = 0.0D;
+				d3 = 0.0D;
+				d4 = -1.0D;
+			}
+
+			double d5 = Math.cos(d1);
+			double d6 = this.interpolateValue(entity.prevPosX, entity.posX, (double) partialTicks) - d2 * 0.7D
+					- d3 * 0.5D * d5;
+			double d7 = this.interpolateValue(entity.prevPosY + (double) entity.getEyeHeight() * 0.7D,
+					entity.posY + (double) entity.getEyeHeight() * 0.7D, (double) partialTicks) - d4 * 0.5D - 0.25D;
+			double d8 = this.interpolateValue(entity.prevPosZ, entity.posZ, (double) partialTicks) - d3 * 0.7D
+					+ d2 * 0.5D * d5;
+			double d9 = this.interpolateValue((double) entityLivingIn.prevRenderYawOffset,
+					(double) entityLivingIn.renderYawOffset, (double) partialTicks) * 0.01745329238474369D
+					+ (Math.PI / 2D);
+			d2 = Math.cos(d9) * (double) entityLivingIn.width * 0.4D;
+			d3 = Math.sin(d9) * (double) entityLivingIn.width * 0.4D;
+			double d10 = this.interpolateValue(entityLivingIn.prevPosX, entityLivingIn.posX, (double) partialTicks)
+					+ d2;
+			double d11 = this.interpolateValue(entityLivingIn.prevPosY, entityLivingIn.posY, (double) partialTicks);
+			double d12 = this.interpolateValue(entityLivingIn.prevPosZ, entityLivingIn.posZ, (double) partialTicks)
+					+ d3;
+			x = x + d2;
+			z = z + d3;
+			double d13 = (double) ((float) (d6 - d10));
+			double d14 = (double) ((float) (d7 - d11));
+			double d15 = (double) ((float) (d8 - d12));
+			GlStateManager.disableTexture2D();
+			GlStateManager.disableLighting();
+			GlStateManager.disableCull();
+			bufferbuilder.begin(5, DefaultVertexFormats.POSITION_COLOR);
+
+			for (int j = 0; j <= 24; ++j) {
+				float f = 0.5F;
+				float f1 = 0.4F;
+				float f2 = 0.3F;
+
+				if (j % 2 == 0) {
+					f *= 0.7F;
+					f1 *= 0.7F;
+					f2 *= 0.7F;
+				}
+
+				float f3 = (float) j / 24.0F;
+				bufferbuilder
+						.pos(x + d13 * (double) f3 + 0.0D,
+								y + d14 * (double) (f3 * f3 + f3) * 0.5D
+										+ (double) ((24.0F - (float) j) / 18.0F + 0.125F),
+								z + d15 * (double) f3)
+						.color(f, f1, f2, 1.0F).endVertex();
+				bufferbuilder
+						.pos(x + d13 * (double) f3 + 0.025D,
+								y + d14 * (double) (f3 * f3 + f3) * 0.5D
+										+ (double) ((24.0F - (float) j) / 18.0F + 0.125F) + 0.025D,
+								z + d15 * (double) f3)
+						.color(f, f1, f2, 1.0F).endVertex();
+			}
+
+			tessellator.draw();
+			bufferbuilder.begin(5, DefaultVertexFormats.POSITION_COLOR);
+
+			for (int k = 0; k <= 24; ++k) {
+				float f4 = 0.5F;
+				float f5 = 0.4F;
+				float f6 = 0.3F;
+
+				if (k % 2 == 0) {
+					f4 *= 0.7F;
+					f5 *= 0.7F;
+					f6 *= 0.7F;
+				}
+
+				float f7 = (float) k / 24.0F;
+				bufferbuilder
+						.pos(x + d13 * (double) f7 + 0.0D,
+								y + d14 * (double) (f7 * f7 + f7) * 0.5D
+										+ (double) ((24.0F - (float) k) / 18.0F + 0.125F) + 0.025D,
+								z + d15 * (double) f7)
+						.color(f4, f5, f6, 1.0F).endVertex();
+				bufferbuilder.pos(x + d13 * (double) f7 + 0.025D,
+						y + d14 * (double) (f7 * f7 + f7) * 0.5D + (double) ((24.0F - (float) k) / 18.0F + 0.125F),
+						z + d15 * (double) f7 + 0.025D).color(f4, f5, f6, 1.0F).endVertex();
+			}
+
+			tessellator.draw();
+			GlStateManager.enableLighting();
+			GlStateManager.enableTexture2D();
+			GlStateManager.enableCull();
+		}
+	}
+
+	private double interpolateValue(double start, double end, double pct) {
+		return start + (end - start) * pct;
+	}
 }
