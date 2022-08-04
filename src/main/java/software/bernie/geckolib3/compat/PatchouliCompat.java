a7b800f4bb6cf87857b09329c15218893b5f759e
AzureZhen <rcraft@cyberconnects2.com>
2022-08-04 12:50:16 -0400

    v3.1.9

--- src/main/java/software/bernie/geckolib3/compat/PatchouliCompat.java
+++ src/main/java/software/bernie/geckolib3/compat/PatchouliCompat.java
@@ -1,23 +1,28 @@
 package software.bernie.geckolib3.compat;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
+import vazkii.patchouli.client.book.gui.GuiBook;
 
 public class PatchouliCompat {
 
-	@SuppressWarnings("resource")
+	@SuppressWarnings({ "resource", "unlikely-arg-type" })
 	public static void patchouliLoaded(MatrixStack matrixStackIn) {
-////	    Patchouli doesnt exist on fabric 1.17 snapshots :p
-//		Class<GuiBook> patchouli = GuiBook.class;
-//		boolean screen = MinecraftClient.getInstance().inGameHud.equals(patchouli);
-//		if (screen) {
-//			matrixStackIn.push();
-//			VertexConsumerProvider.Immediate irendertypebuffer$impl = MinecraftClient.getInstance().getBufferBuilders()
-//					.getEntityVertexConsumers();
-//			DiffuseLighting.disableGuiDepthLighting();
-//			irendertypebuffer$impl.draw();
-//			RenderSystem.enableDepthTest();
-//			DiffuseLighting.enableGuiDepthLighting();
-//			matrixStackIn.pop();
-//		}
+		Class<GuiBook> patchouli = GuiBook.class;
+		boolean screen = MinecraftClient.getInstance().inGameHud.equals(patchouli);
+		if (screen) {
+			matrixStackIn.push();
+			VertexConsumerProvider.Immediate irendertypebuffer$impl = MinecraftClient.getInstance().getBufferBuilders()
+					.getEntityVertexConsumers();
+			DiffuseLighting.disableGuiDepthLighting();
+			irendertypebuffer$impl.draw();
+			RenderSystem.enableDepthTest();
+			DiffuseLighting.enableGuiDepthLighting();
+			matrixStackIn.pop();
+		}
 	}
 }
