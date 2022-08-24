f17b1f766a3bc4f2aa7083eaea54abffc2114702
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-08-24 14:45:17 -0400

    v3.1.18

--- src/main/java/software/bernie/geckolib3/renderers/geo/ExtendedGeoEntityRenderer.java
+++ src/main/java/software/bernie/geckolib3/renderers/geo/ExtendedGeoEntityRenderer.java
@@ -424,15 +424,21 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 		sourceLimb.setPos(-(bone.getPivotX() - ((bone.getPivotX() * scaleX) - bone.getPivotX()) / scaleX),
 				-(bone.getPivotY() - ((bone.getPivotY() * scaleY) - bone.getPivotY()) / scaleY),
 				(bone.getPivotZ() - ((bone.getPivotZ() * scaleZ) - bone.getPivotZ()) / scaleZ));
-		
+
 		if (!geoArmor) {
 			sourceLimb.xRot = -bone.getRotationX();
 			sourceLimb.yRot = -bone.getRotationY();
 			sourceLimb.zRot = bone.getRotationZ();
 		} else {
+			// All those *= 2 calls ARE necessary, otherwise the geo armor will apply
+			// rotations twice, so to have it only applied one time in the correct direction
+			// we add 2x the negative rotation to it
 			float xRot = -bone.getRotationX();
+			xRot *= 2;
 			float yRot = -bone.getRotationY();
+			yRot *= 2;
 			float zRot = bone.getRotationZ();
+			zRot *= 2;
 			GeoBone tmpBone = bone.parent;
 			while (tmpBone != null) {
 				xRot -= tmpBone.getRotationX();
