2c3970a688f407d8a28974e75fc73cfc9f1746da
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-10-06 01:05:30 -0400

    Use Getter/Setting methods for RTB

--- Fabric/src/main/java/software/bernie/geckolib3/renderer/geo/ExtendedGeoEntityRenderer.java
+++ Fabric/src/main/java/software/bernie/geckolib3/renderer/geo/ExtendedGeoEntityRenderer.java
@@ -94,7 +94,6 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 	protected float heightScale;
 
 	protected T currentEntityBeingRendered;
-	protected VertexConsumerProvider rtb;
 
 	protected float currentPartialTicks;
 	protected Identifier textureForBone = null;
@@ -212,7 +211,6 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 	public void renderEarly(T animatable, MatrixStack stackIn, float ticks, VertexConsumerProvider renderTypeBuffer,
 			VertexConsumer vertexBuilder, int packedLightIn, int packedOverlayIn, float red, float green, float blue,
 			float partialTicks) {
-		this.rtb = renderTypeBuffer;
 		super.renderEarly(animatable, stackIn, ticks, renderTypeBuffer, vertexBuilder, packedLightIn, packedOverlayIn,
 				red, green, blue, partialTicks);
 		if (this.getCurrentModelRenderCycle() == EModelRenderCycle.INITIAL /* Pre-Layers */) {
@@ -438,7 +436,7 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 	@Override
 	public void renderRecursively(GeoBone bone, MatrixStack stack, VertexConsumer bufferIn, int packedLightIn,
 			int packedOverlayIn, float red, float green, float blue, float alpha) {
-		if (this.rtb == null) {
+		if (this.getCurrentRTB() == null) {
 			throw new IllegalStateException("RenderTypeBuffer must never be null at this point!");
 		}
 
@@ -449,10 +447,10 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 
 		final RenderLayer rt = customTextureMarker
 				? this.getRenderTypeForBone(bone, this.currentEntityBeingRendered, this.currentPartialTicks, stack,
-						bufferIn, this.rtb, packedLightIn, this.textureForBone)
-				: this.getRenderType(this.currentEntityBeingRendered, this.currentPartialTicks, stack, this.rtb,
+						bufferIn, this.getCurrentRTB(), packedLightIn, this.textureForBone)
+				: this.getRenderType(this.currentEntityBeingRendered, this.currentPartialTicks, stack, this.getCurrentRTB(),
 						bufferIn, packedLightIn, currentTexture);
-		bufferIn = this.rtb.getBuffer(rt);
+		bufferIn = this.getCurrentRTB().getBuffer(rt);
 
 		if (this.getCurrentModelRenderCycle() == EModelRenderCycle.INITIAL) {
 			stack.push();
@@ -464,7 +462,7 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 				stack.pop();
 
 				// Reset buffer...
-				bufferIn = this.rtb.getBuffer(rt);
+				bufferIn = this.getCurrentRTB().getBuffer(rt);
 			} else {
 				ItemStack boneItem = this.getHeldItemForBone(bone.getName(), this.currentEntityBeingRendered);
 				BlockState boneBlock = this.getHeldBlockForBone(bone.getName(), this.currentEntityBeingRendered);
@@ -473,7 +471,7 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 					this.handleItemAndBlockBoneRendering(stack, bone, boneItem, boneBlock, packedLightIn);
 					stack.pop();
 
-					bufferIn = this.rtb.getBuffer(rt);
+					bufferIn = this.getCurrentRTB().getBuffer(rt);
 				}
 			}
 			stack.pop();
@@ -488,7 +486,7 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 		//////////////////////////////////////
 		// reset buffer
 		if (customTextureMarker) {
-			bufferIn = this.rtb.getBuffer(this.getRenderType(currentEntityBeingRendered, this.currentPartialTicks,
+			bufferIn = this.getCurrentRTB().getBuffer(this.getRenderType(currentEntityBeingRendered, this.currentPartialTicks,
 					stack, rtb, bufferIn, packedLightIn, currentTexture));
 			// Reset the marker...
 			this.textureForBone = null;
@@ -521,14 +519,14 @@ public abstract class ExtendedGeoEntityRenderer<T extends LivingEntity & IAnimat
 		if (boneItem != null) {
 			this.preRenderItem(stack, boneItem, bone.getName(), this.currentEntityBeingRendered, bone);
 
-			this.renderItemStack(stack, this.rtb, packedLightIn, boneItem, bone.getName());
+			this.renderItemStack(stack, this.getCurrentRTB(), packedLightIn, boneItem, bone.getName());
 
 			this.postRenderItem(stack, boneItem, bone.getName(), this.currentEntityBeingRendered, bone);
 		}
 		if (boneBlock != null) {
 			this.preRenderBlock(stack, boneBlock, bone.getName(), this.currentEntityBeingRendered);
 
-			this.renderBlock(stack, this.rtb, packedLightIn, boneBlock);
+			this.renderBlock(stack, this.getCurrentRTB(), packedLightIn, boneBlock);
 
 			this.postRenderBlock(stack, boneBlock, bone.getName(), this.currentEntityBeingRendered);
 		}
