903de2ce231f7a308e799145bb8c9ceac2e759f9
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-10-22 16:41:11 -0400

    Pushing to create new branch with changes already in

--- Quilt/src/main/java/software/bernie/geckolib3/resource/data/GlowingMetadataSectionSerializer.java
+++ Quilt/src/main/java/software/bernie/geckolib3/resource/data/GlowingMetadataSectionSerializer.java
@@ -26,9 +26,16 @@ public class GlowingMetadataSectionSerializer implements MetadataSectionSerializ
 	public GlowingMetadataSection fromJson(JsonObject jsonobject) {
 		if (jsonobject.has("sections")) {
 			JsonArray jsonarray = GsonHelper.convertToJsonArray(jsonobject.get("sections"), "sections");
-			GlowingMetadataSection result = new GlowingMetadataSection(JsonUtil.stream(jsonarray, JsonObject.class)
-					.map(jsonObj -> new Section(GsonHelper.getAsInt(jsonObj, "x1"), GsonHelper.getAsInt(jsonObj, "y1"),
-							GsonHelper.getAsInt(jsonObj, "x2"), GsonHelper.getAsInt(jsonObj, "y2"))));
+			GlowingMetadataSection result = new GlowingMetadataSection(
+					JsonUtil.stream(jsonarray, JsonObject.class).map(jsonObj -> {
+						final int x1 = GsonHelper.getAsInt(jsonObj, "x1", GsonHelper.getAsInt(jsonObj, "x", 0));
+						final int y1 = GsonHelper.getAsInt(jsonObj, "y1", GsonHelper.getAsInt(jsonObj, "y", 0));
+						final int width = GsonHelper.getAsInt(jsonObj, "w", 0);
+						final int height = GsonHelper.getAsInt(jsonObj, "h", 0);
+						final int x2 = GsonHelper.getAsInt(jsonObj, "x2", x1 + width);
+						final int y2 = GsonHelper.getAsInt(jsonObj, "y2", y1 + height);
+						return new Section(x1, y1, x2, y2);
+					}));
 			if (!result.isEmpty()) {
 				return result;
 			}
