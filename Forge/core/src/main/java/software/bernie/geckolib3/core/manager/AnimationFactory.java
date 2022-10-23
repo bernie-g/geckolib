86c171760ddaf9acced13375c058c76a8760b32c
Tslat <tslatmail@gmail.com>
2022-10-23 12:43:06 +1000

    Split AnimationFactory implementation into Instanced and Singleton handlers for more efficient and sensible factories AnimationFactory instantiation moved to AnimationFactory#create

--- Forge/core/src/main/java/software/bernie/geckolib3/core/manager/AnimationFactory.java
+++ Forge/core/src/main/java/software/bernie/geckolib3/core/manager/AnimationFactory.java
@@ -1,33 +1,29 @@
 package software.bernie.geckolib3.core.manager;
 
-import java.util.HashMap;
-
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import software.bernie.geckolib3.core.IAnimatable;
 
-public class AnimationFactory {
-	private final IAnimatable animatable;
-	private HashMap<Integer, AnimationData> animationDataMap = new HashMap<>();
+public abstract class AnimationFactory {
+	protected final IAnimatable animatable;
 
-	public AnimationFactory(IAnimatable animatable) {
+	protected AnimationFactory(IAnimatable animatable) {
 		this.animatable = animatable;
 	}
 
 	/**
 	 * This creates or gets the cached animation manager for any unique ID. For
 	 * itemstacks, this is typically a hashcode of their nbt. For entities it should
-	 * be their unique uuid. For tile entities you can use nbt or just one constant
+	 * be their unique uuid. For block entities you can use nbt or just one constant
 	 * value since they are not singletons.
 	 *
 	 * @param uniqueID A unique integer ID. For every ID the same animation manager
 	 *                 will be returned.
 	 * @return the animatable manager
 	 */
-	public AnimationData getOrCreateAnimationData(Integer uniqueID) {
-		if (!animationDataMap.containsKey(uniqueID)) {
-			AnimationData data = new AnimationData();
-			animatable.registerControllers(data);
-			animationDataMap.put(uniqueID, data);
-		}
-		return animationDataMap.get(uniqueID);
+	public abstract AnimationData getOrCreateAnimationData(Integer uniqueID);
+
+	public static AnimationFactory create(IAnimatable animatable) {
+		return animatable instanceof Entity || animatable instanceof BlockEntity ? new InstancedAnimationFactory(animatable) : new SingletonAnimationFactory(animatable);
 	}
 }
