33cfab3e16f80cc28b72dd7a59c6044e937cea23
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-10-31 16:36:19 -0400

    Remove Forge and Quilt/ Move over latest state of Forge 1.19.2 when ready for 1.19.3.

--- Forge/src/main/java/software/bernie/geckolib3/network/ISyncable.java
+++
@@ -1,9 +0,0 @@
-package software.bernie.geckolib3.network;
-
-public interface ISyncable {
-    void onAnimationSync(int id, int state);
-
-    default String getSyncKey() {
-        return this.getClass().getName();
-    }
-}
