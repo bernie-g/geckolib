19bb7447dbb4babd6813ba7ebabd98be3ac28251
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-10 20:49:36 -0500

    clean branch

--- Forge/core/src/main/java/software/bernie/geckolib3/core/util/Axis.java
+++
@@ -1,5 +0,0 @@
-package software.bernie.geckolib3.core.util;
-
-public enum Axis {
-	X, Y, Z
-}
--- Quilt/src/main/java/software/bernie/geckolib3/model/provider/data/EntityModelData.java
+++
@@ -1,8 +0,0 @@
-package software.bernie.geckolib3.model.provider.data;
-
-public class EntityModelData {
-	public boolean isSitting;
-	public boolean isChild;
-	public float netHeadYaw;
-	public float headPitch;
-}
