589df03d9013b169e5c7fe563df850017f0c5d35
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-23 14:37:22 -0500

    Updates to new SoundEvent

--- Fabric/src/main/java/software/bernie/example/registry/SoundRegistry.java
+++ Fabric/src/main/java/software/bernie/example/registry/SoundRegistry.java
@@ -9,6 +9,6 @@ import software.bernie.geckolib.GeckoLib;
 public final class SoundRegistry {
 
 	public static SoundEvent JACK_MUSIC = Registry.register(BuiltInRegistries.SOUND_EVENT, "jack_in_the_box_music",
-			new SoundEvent(new ResourceLocation(GeckoLib.ModID, "jack_in_the_box_music")));
+			SoundEvent.createFixedRangeEvent(new ResourceLocation(GeckoLib.ModID, "jack_in_the_box_music"), 0));
 
 }
