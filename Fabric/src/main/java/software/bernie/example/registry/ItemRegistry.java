9b30482232d1870a04d189b85eaf07c3e116606b
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-23 14:03:56 -0500

    Fabric spawn eggs

--- Fabric/src/main/java/software/bernie/example/registry/ItemRegistry.java
+++ Fabric/src/main/java/software/bernie/example/registry/ItemRegistry.java
@@ -26,7 +26,15 @@ public class ItemRegistry {
     public static final WolfArmorItem WOLF_ARMOR_CHESTPLATE = registerItem("wolf_armor_chestplate", new WolfArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.CHEST, new Item.Properties()));
     public static final WolfArmorItem WOLF_ARMOR_LEGGINGS = registerItem("wolf_armor_leggings", new WolfArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.LEGS, new Item.Properties()));
     public static final WolfArmorItem WOLF_ARMOR_BOOTS = registerItem("wolf_armor_boots", new WolfArmorItem(ArmorMaterials.DIAMOND, EquipmentSlot.FEET, new Item.Properties()));
-
+    
+    public static final SpawnEggItem BAT_SPAWN_EGG = registerItem("bat_spawn_egg", new SpawnEggItem(EntityRegistry.BAT, 0x1F1F1F, 0x0D0D0D, new Item.Properties()));
+    public static final SpawnEggItem BIKE_SPAWN_EGG = registerItem("bike_spawn_egg", new SpawnEggItem(EntityRegistry.BIKE, 0xD3E3E6, 0xE9F1F5, new Item.Properties()));
+    public static final SpawnEggItem RACE_CAR_SPAWN_EGG = registerItem("race_car_spawn_egg", new SpawnEggItem(EntityRegistry.RACE_CAR, 0x9E1616, 0x595959, new Item.Properties()));
+    public static final SpawnEggItem PARASITE_SPAWN_EGG = registerItem("parasite_spawn_egg", new SpawnEggItem(EntityRegistry.PARASITE, 0x302219, 0xACACAC, new Item.Properties()));
+    public static final SpawnEggItem MUTANT_ZOMBIE_SPAWN_EGG = registerItem("mutant_zombie_spawn_egg", new SpawnEggItem(EntityRegistry.MUTANT_ZOMBIE, 0x3C6236, 0x579989, new Item.Properties()));
+    public static final SpawnEggItem FAKE_GLASS_SPAWN_EGG = registerItem("fake_glass_spawn_egg", new SpawnEggItem(EntityRegistry.FAKE_GLASS, 0xDD0000, 0xD8FFF7, new Item.Properties()));
+    public static final SpawnEggItem COOL_KID_SPAWN_EGG = registerItem("cool_kid_spawn_egg", new SpawnEggItem(EntityRegistry.COOL_KID, 0x5F2A31, 0x6F363E, new Item.Properties()));
+    
     public static final CreativeModeTab ITEMS_GROUP = FabricItemGroup
             .builder(new ResourceLocation(GeckoLib.ModID, "geckolib_examples"))
             .icon(() -> new ItemStack(ItemRegistry.JACK_IN_THE_BOX))
@@ -43,6 +51,13 @@ public class ItemRegistry {
                 entries.accept(ItemRegistry.WOLF_ARMOR_BOOTS);
                 entries.accept(BlockRegistry.GECKO_HABITAT_BLOCK);
                 entries.accept(BlockRegistry.FERTILIZER_BLOCK);
+                entries.accept(ItemRegistry.BAT_SPAWN_EGG);
+                entries.accept(ItemRegistry.BIKE_SPAWN_EGG);
+                entries.accept(ItemRegistry.RACE_CAR_SPAWN_EGG);
+                entries.accept(ItemRegistry.PARASITE_SPAWN_EGG);
+                entries.accept(ItemRegistry.MUTANT_ZOMBIE_SPAWN_EGG);
+                entries.accept(ItemRegistry.FAKE_GLASS_SPAWN_EGG);
+                entries.accept(ItemRegistry.COOL_KID_SPAWN_EGG);
             }).build();
 
     public static <I extends Item> I registerItem(String name, I item) {
