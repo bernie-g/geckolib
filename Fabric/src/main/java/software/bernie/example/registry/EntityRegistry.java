ec4b6e073cd923916b5f6e9567f904f5a5265215
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-22 19:20:05 -0500

    Fixes crash with entities

--- Fabric/src/main/java/software/bernie/example/registry/EntityRegistry.java
+++ Fabric/src/main/java/software/bernie/example/registry/EntityRegistry.java
@@ -1,13 +1,23 @@
 package software.bernie.example.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
-import software.bernie.example.entity.*;
+import software.bernie.example.entity.BatEntity;
+import software.bernie.example.entity.BikeEntity;
+import software.bernie.example.entity.CoolKidEntity;
+import software.bernie.example.entity.FakeGlassEntity;
+import software.bernie.example.entity.MutantZombieEntity;
+import software.bernie.example.entity.ParasiteEntity;
+import software.bernie.example.entity.RaceCarEntity;
+import software.bernie.geckolib.GeckoLib;
 
 public class EntityRegistry {
 
@@ -31,7 +41,8 @@ public class EntityRegistry {
 
     public static <T extends Mob> EntityType<T> registerMob(String name, EntityType.EntityFactory<T> entity,
                                                             float width, float height, int primaryEggColor, int secondaryEggColor) {
-        EntityType<T> entityType = FabricEntityTypeBuilder.<T>create(MobCategory.CREATURE).entityFactory(entity).dimensions(new EntityDimensions(width, height, false)).build();
+    	EntityType<T> entityType = Registry.register(BuiltInRegistries.ENTITY_TYPE,
+    			new ResourceLocation(GeckoLib.ModID, name),FabricEntityTypeBuilder.create(MobCategory.CREATURE, entity).dimensions(EntityDimensions.scalable(width, height)).build());
         ItemRegistry.registerItem(name + "_spawn_egg", new SpawnEggItem(entityType, primaryEggColor, secondaryEggColor, new Item.Properties()));
         return entityType;
     }
