10c5d1fbc3976272ee8896caf9e562854f4b1140
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-12-09 05:52:47 -0500

    Fixes Tab name and item being blank.

--- Forge/src/main/java/software/bernie/example/GeckoLibMod.java
+++ Forge/src/main/java/software/bernie/example/GeckoLibMod.java
@@ -5,6 +5,7 @@
 
 package software.bernie.example;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -39,27 +40,28 @@ public final class GeckoLibMod {
 	public void addCreativeTabs(final CreativeModeTabEvent.Register event) {
 		event.registerCreativeModeTab(new ResourceLocation(GeckoLib.MOD_ID, "geckolib_examples"),
 				e -> e.icon(() -> new ItemStack(ItemRegistry.JACK_IN_THE_BOX.get()))
-						.displayItems((enabledFeatures, entries, operatorEnabled) -> {
-			                entries.accept(ItemRegistry.JACK_IN_THE_BOX.get());
-			                entries.accept(ItemRegistry.PISTOL.get());
-			                entries.accept(ItemRegistry.GECKO_ARMOR_HELMET.get());
-			                entries.accept(ItemRegistry.GECKO_ARMOR_CHESTPLATE.get());
-			                entries.accept(ItemRegistry.GECKO_ARMOR_LEGGINGS.get());
-			                entries.accept(ItemRegistry.GECKO_ARMOR_BOOTS.get());
-			                entries.accept(ItemRegistry.WOLF_ARMOR_HELMET.get());
-			                entries.accept(ItemRegistry.WOLF_ARMOR_CHESTPLATE.get());
-			                entries.accept(ItemRegistry.WOLF_ARMOR_LEGGINGS.get());
-			                entries.accept(ItemRegistry.WOLF_ARMOR_BOOTS.get());
-			                entries.accept(ItemRegistry.GECKO_HABITAT.get());
-			                entries.accept(ItemRegistry.FERTILIZER.get());
-			                entries.accept(ItemRegistry.BAT_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.BIKE_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.RACE_CAR_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.PARASITE_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.MUTANT_ZOMBIE_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.GREMLIN_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.FAKE_GLASS_SPAWN_EGG.get());
-			                entries.accept(ItemRegistry.COOL_KID_SPAWN_EGG.get());
+						.title(Component.translatable("itemGroup." + GeckoLib.MOD_ID + ".geckolib_examples"))
+							.displayItems((enabledFeatures, entries, operatorEnabled) -> {
+				                entries.accept(ItemRegistry.JACK_IN_THE_BOX.get());
+				                entries.accept(ItemRegistry.PISTOL.get());
+				                entries.accept(ItemRegistry.GECKO_ARMOR_HELMET.get());
+				                entries.accept(ItemRegistry.GECKO_ARMOR_CHESTPLATE.get());
+				                entries.accept(ItemRegistry.GECKO_ARMOR_LEGGINGS.get());
+				                entries.accept(ItemRegistry.GECKO_ARMOR_BOOTS.get());
+				                entries.accept(ItemRegistry.WOLF_ARMOR_HELMET.get());
+				                entries.accept(ItemRegistry.WOLF_ARMOR_CHESTPLATE.get());
+				                entries.accept(ItemRegistry.WOLF_ARMOR_LEGGINGS.get());
+				                entries.accept(ItemRegistry.WOLF_ARMOR_BOOTS.get());
+				                entries.accept(ItemRegistry.GECKO_HABITAT.get());
+				                entries.accept(ItemRegistry.FERTILIZER.get());
+				                entries.accept(ItemRegistry.BAT_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.BIKE_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.RACE_CAR_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.PARASITE_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.MUTANT_ZOMBIE_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.GREMLIN_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.FAKE_GLASS_SPAWN_EGG.get());
+				                entries.accept(ItemRegistry.COOL_KID_SPAWN_EGG.get());
 				}));
 	}
 
