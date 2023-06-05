6e8c75e1a2ded123fd7b654db1698e1be0fa053b
Tslat <tslatmail@gmail.com>
2023-06-05 15:37:32 +1000

    Remove explicit Quilt support

--- Quilt/core/src/main/java/software/bernie/geckolib/core/object/PlayState.java
+++
@@ -1,9 +0,0 @@
-package software.bernie.geckolib.core.object;
-
-/**
- * State enum to define whether an {@link software.bernie.geckolib.core.animation.AnimationController} should continue or stop
- */
-public enum PlayState {
-	CONTINUE,
-	STOP
-}
