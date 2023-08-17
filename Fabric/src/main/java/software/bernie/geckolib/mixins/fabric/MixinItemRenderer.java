282d136457a58484c863310e0a70f1cc5e413523
Tslat <tslatmail@gmail.com>
2023-08-17 17:12:02 +1000

    Fix double-rendering of BEWLRs #465

--- Fabric/src/main/java/software/bernie/geckolib/mixins/fabric/MixinItemRenderer.java
+++ Fabric/src/main/java/software/bernie/geckolib/mixins/fabric/MixinItemRenderer.java
@@ -1,6 +1,7 @@
 package software.bernie.geckolib.mixins.fabric;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
@@ -9,6 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import software.bernie.geckolib.animatable.client.RenderProvider;
 
@@ -17,8 +19,19 @@ import software.bernie.geckolib.animatable.client.RenderProvider;
  */
 @Mixin(ItemRenderer.class)
 public class MixinItemRenderer {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"), cancellable = true)
+    // Done in two parts so that the cancellation of the vanilla render can fail-safe without breaking functionality
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"))
     public void itemModelHook(ItemStack itemStack, ItemDisplayContext transformType, boolean bl, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo ci){
-        RenderProvider.of(itemStack).getCustomRenderer().renderByItem(itemStack, transformType, poseStack, multiBufferSource, i, j);
+       final BlockEntityWithoutLevelRenderer renderer = RenderProvider.of(itemStack).getCustomRenderer();
+
+       if (renderer != RenderProvider.DEFAULT.getCustomRenderer())
+           renderer.renderByItem(itemStack, transformType, poseStack, multiBufferSource, i, j);
+    }
+
+    @Redirect(method = "render", require = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"))
+    public void cancelRender(BlockEntityWithoutLevelRenderer renderer, ItemStack itemStack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource bufferSource, int i, int j) {
+        if (RenderProvider.of(itemStack).getCustomRenderer() == renderer)
+            renderer.renderByItem(itemStack, displayContext, poseStack, bufferSource, i, j);
     }
 }
