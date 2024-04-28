b886469ba85b0835364ff6878d9fe44d85f145a3
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2024-04-28 18:12:36 +0200

    Cleanup GeometryTree

--- common/src/main/java/software/bernie/geckolib/loading/object/GeometryTree.java
+++ common/src/main/java/software/bernie/geckolib/loading/object/GeometryTree.java
@@ -1,71 +1,34 @@
 package software.bernie.geckolib.loading.object;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import software.bernie.geckolib.loading.json.raw.Bone;
 import software.bernie.geckolib.loading.json.raw.MinecraftGeometry;
 import software.bernie.geckolib.loading.json.raw.Model;
 import software.bernie.geckolib.loading.json.raw.ModelProperties;
 
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 /**
  * Container class for a {@link Bone} structure, used at startup during deserialization
  */
 public record GeometryTree(Map<String, BoneStructure> topLevelBones, ModelProperties properties) {
-	public static GeometryTree fromModel(Model model) {
+
+	public static GeometryTree fromModel(Model model){
 		final Map<String, BoneStructure> topLevelBones = new Object2ObjectOpenHashMap<>();
 		final MinecraftGeometry geometry = model.minecraftGeometry()[0];
-		final List<Bone> bones = new ObjectArrayList<>(geometry.bones());
+		final Bone[] bones = geometry.bones();
+		final Map<String, BoneStructure> lookup = new Object2ObjectOpenHashMap<>();
 
-		while (true) {
-			if (!bones.removeIf(bone -> {
-				if (bone.parent() == null) {
-					topLevelBones.put(bone.name(), new BoneStructure(bone));
-
-					return true;
-				}
-
-				BoneStructure parent = findBoneStructureInTree(topLevelBones, bone.parent());
-
-				if (parent != null) {
-					parent.children().put(bone.name(), new BoneStructure(bone));
-
-					return true;
-				}
-
-				return false;
-			})) {
-				if (!bones.isEmpty()) {
-					StringJoiner joiner = new StringJoiner(", ");
-
-					for (Bone remainingBone : bones) {
-						joiner.add(remainingBone.name() + " -> " + remainingBone.parent());
-					}
-
-					throw new IllegalArgumentException("Invalid model definition. Found bone(s) with undefined parent (child -> parent): " + joiner);
-				}
-
-				break;
+		for (Bone bone : bones) {
+			final String parentName = bone.parent();
+			final String boneName = bone.name();
+			final BoneStructure boneStructure = lookup.computeIfAbsent(boneName, key -> new BoneStructure(bone));
+			if (parentName == null) {
+				topLevelBones.put(boneName, boneStructure);
+			} else {
+				lookup.get(parentName).children().put(boneName, boneStructure);
 			}
 		}
-
 		return new GeometryTree(topLevelBones, geometry.modelProperties());
-	}
-
-	private static BoneStructure findBoneStructureInTree(Map<String, BoneStructure> bones, String boneName) {
-		for (BoneStructure entry : bones.values()) {
-			if (boneName.equals(entry.self().name()))
-				return entry;
-
-			BoneStructure subStructure = findBoneStructureInTree(entry.children(), boneName);
-
-			if (subStructure != null)
-				return subStructure;
-		}
-
-		return null;
 	}
 }
