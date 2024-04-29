01f787ecc55619cc785d1370f6489396eebf5779
Tslat <tslatmail@gmail.com>
2024-04-30 06:02:03 +1000

    Cleanup & re-add exception to model loading

--- common/src/main/java/software/bernie/geckolib/loading/object/GeometryTree.java
+++ common/src/main/java/software/bernie/geckolib/loading/object/GeometryTree.java
@@ -12,26 +12,35 @@ import java.util.Map;
  * Container class for a {@link Bone} structure, used at startup during deserialization
  */
 public record GeometryTree(Map<String, BoneStructure> topLevelBones, ModelProperties properties) {
-
-	public static GeometryTree fromModel(Model model){
+	public static GeometryTree fromModel(Model model) {
 		final Map<String, BoneStructure> topLevelBones = new Object2ObjectOpenHashMap<>();
 		final MinecraftGeometry geometry = model.minecraftGeometry()[0];
 		final Bone[] bones = geometry.bones();
-		final Map<String, BoneStructure> lookup = new Object2ObjectOpenHashMap<>();
+		final Map<String, BoneStructure> lookup = new Object2ObjectOpenHashMap<>(bones.length);
+
 		for (Bone bone : bones) {
 			final BoneStructure boneStructure = new BoneStructure(bone);
+
 			lookup.put(bone.name(), boneStructure);
+
 			if (bone.parent() == null)
 				topLevelBones.put(bone.name(), boneStructure);
 		}
+
 		for (Bone bone : bones) {
 			final String parentName = bone.parent();
-			final String boneName = bone.name();
-			final BoneStructure boneStructure = lookup.get(boneName);
+
 			if (parentName != null) {
-				lookup.get(parentName).children().put(boneName, boneStructure);
+				final String boneName = bone.name();
+				final BoneStructure parentStructure = lookup.get(parentName);
+
+				if (parentStructure == null)
+					throw new IllegalArgumentException("Invalid model definition. Found bone with undefined parent (child -> parent): " + boneName + " -> " + parentName);
+
+				parentStructure.children().put(boneName, lookup.get(boneName));
 			}
 		}
+
 		return new GeometryTree(topLevelBones, geometry.modelProperties());
 	}
 }
