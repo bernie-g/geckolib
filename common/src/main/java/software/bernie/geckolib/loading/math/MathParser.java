b73b6633cf0c22266895ff195e05631c2ca1fce9
Tslat <tslatmail@gmail.com>
2024-08-24 09:07:38 +1000

    Add further precision to variable detection

--- common/src/main/java/software/bernie/geckolib/loading/math/MathParser.java
+++ common/src/main/java/software/bernie/geckolib/loading/math/MathParser.java
@@ -591,7 +591,10 @@ public class MathParser {
      * Functionally this is just a confirmation-by-elimination check, since names don't really have a defined form
      */
     protected static boolean isLikelyVariable(String string) {
-        return !isNumeric(string) && !isFunctionRegistered(string) && !Operator.isOperator(string);
+        if (VARIABLES.containsKey(string))
+            return true;
+
+        return !isNumeric(string) && !isFunctionRegistered(string) && !Operator.isOperator(string) && !string.equals("?") && !string.equals(":");
     }
 
     /**
