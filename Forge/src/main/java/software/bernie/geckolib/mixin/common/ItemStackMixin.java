06b183f1fd55c416a8ae952ca847f3417697bdc3
Tslat <tslatmail@gmail.com>
2025-03-23 09:24:45 +1000

    Rollback stability change (yay forge)

--- Forge/src/main/java/software/bernie/geckolib/mixin/common/ItemStackMixin.java
+++ Forge/src/main/java/software/bernie/geckolib/mixin/common/ItemStackMixin.java
@@ -3,21 +3,15 @@ package software.bernie.geckolib.mixin.common;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import software.bernie.geckolib.animatable.GeoItem;
-
-import java.util.Objects;
 
 /**
  * Injection into ItemStack functionality to handle duplication and splitting with GeckoLib stack identifiers
  */
 @Mixin(ItemStack.class)
-public class ItemStackMixin {
-    /**
+public class ItemStackMixin {/*
+    *//**
      * Remove the GeckoLib stack ID when splitting up a stack into two
-     */
+     *//*
     @Redirect(method = "split", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;copyWithCount(I)Lnet/minecraft/world/item/ItemStack;"))
     public ItemStack geckolib$removeGeckolibIdOnCopy(ItemStack instance, int count) {
         ItemStack copy = instance.copyWithCount(count);
@@ -28,9 +22,9 @@ public class ItemStackMixin {
         return copy;
     }
 
-    /**
+    *//**
      * Consider ItemStacks equal if the only difference is their GeckoLib stack ID
-     */
+     *//*
     @Redirect(method = "isSameItemSameTags", at = @At(value = "INVOKE", target = "Ljava/util/Objects;equals(Ljava/lang/Object;Ljava/lang/Object;)Z"))
     private static boolean geckolib$skipGeckolibIdOnCompare(Object a, Object b) {
         if (Objects.equals(a, b))
@@ -42,10 +36,10 @@ public class ItemStackMixin {
         return geckolib$areTagsMatchingIgnoringGeckoLibId(tag1, tag2);
     }
 
-    /**
+    *//**
      * Perform an {@link Object#equals(Object)} check on two {@link CompoundTag}s,
      * ignoring any GeckoLib stack ids that may be present.
-     */
+     *//*
     @Unique
     private static boolean geckolib$areTagsMatchingIgnoringGeckoLibId(CompoundTag tag1, CompoundTag tag2) {
         boolean patched = false;
@@ -65,5 +59,5 @@ public class ItemStackMixin {
         }
 
         return patched && Objects.equals(tag1, tag2);
-    }
+    }*/
 }
