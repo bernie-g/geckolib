b6c068504808c344554d1c734659e05164eb9e90
Tslat <tslatmail@gmail.com>
2025-03-29 07:55:04 +1000

    Update AnimatableIdCache

--- common/src/main/java/software/bernie/geckolib/cache/AnimatableIdCache.java
+++ common/src/main/java/software/bernie/geckolib/cache/AnimatableIdCache.java
@@ -1,9 +1,11 @@
 package software.bernie.geckolib.cache;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.saveddata.SavedDataType;
+import software.bernie.geckolib.GeckoLibConstants;
 import software.bernie.geckolib.animatable.instance.SingletonAnimatableInstanceCache;
 
 /**
@@ -12,14 +14,23 @@ import software.bernie.geckolib.animatable.instance.SingletonAnimatableInstanceC
  * Generally only used for {@link net.minecraft.world.item.Item Items}, but any {@link SingletonAnimatableInstanceCache singleton} will likely use this.
  */
 public final class AnimatableIdCache extends SavedData {
-	private static final Factory<AnimatableIdCache> FACTORY = new Factory<>(AnimatableIdCache::new, AnimatableIdCache::new, null);
-	private static final String DATA_KEY = "geckolib_id_cache";
+	public static final SavedDataType<AnimatableIdCache> TYPE = new SavedDataType<>(GeckoLibConstants.MODID + "_id_cache", AnimatableIdCache::new, AnimatableIdCache::codec, null);
+
+	private static Codec<AnimatableIdCache> codec(SavedData.Context context) {
+		return RecordCodecBuilder.create(builder -> builder.group(
+				RecordCodecBuilder.point(context.levelOrThrow()),
+				Codec.LONG.fieldOf("last_id").forGetter(cache -> cache.lastId)
+		).apply(builder, AnimatableIdCache::new));
+	}
+
 	private long lastId;
 
-	private AnimatableIdCache() {}
+	private AnimatableIdCache(SavedData.Context context) {
+		this(context.levelOrThrow(), 0);
+	}
 
-	private AnimatableIdCache(CompoundTag tag, HolderLookup.Provider registryLookup) {
-		this.lastId = tag.getLong("last_id");
+	private AnimatableIdCache(ServerLevel level, long lastId) {
+		this.lastId = lastId;
 	}
 
 	/**
@@ -29,7 +40,7 @@ public final class AnimatableIdCache extends SavedData {
 	 * @return The next free ID, which is immediately reserved for use after calling this method
 	 */
 	public static long getFreeId(ServerLevel level) {
-		return getCache(level).getNextId();
+		return getCache(level.getServer().overworld()).getNextId();
 	}
 
 	private long getNextId() {
@@ -38,14 +49,7 @@ public final class AnimatableIdCache extends SavedData {
 		return ++this.lastId;
 	}
 
-	@Override
-	public CompoundTag save(CompoundTag tag, HolderLookup.Provider registryLookup) {
-		tag.putLong("last_id", this.lastId);
-
-		return tag;
-	}
-
 	private static AnimatableIdCache getCache(ServerLevel level) {
-		return level.getServer().overworld().getDataStorage().computeIfAbsent(FACTORY, DATA_KEY);
+		return level.getServer().overworld().getDataStorage().computeIfAbsent(TYPE);
 	}
 }
