751828dd5e4434977d58b58d55cf9e2bf3f42fd3
Tslat <tslatmail@gmail.com>
2025-09-20 11:01:29 +1000

    Remove NeoForge branch - it hasn't been used in a long time and won't ever be

--- NeoForge/src/main/java/software/bernie/geckolib/model/data/EntityModelData.java
+++
@@ -1,6 +0,0 @@
-package software.bernie.geckolib.model.data;
-
-/**
- * Container class for various pieces of data relating to a model's current state.
- */
-public record EntityModelData(boolean isSitting, boolean isChild, float netHeadYaw, float headPitch) {}
