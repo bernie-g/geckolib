9703d1f18b58dab0174e3f85c2bdd860f3ab190f
Tslat <tslatmail@gmail.com>
2025-10-19 11:54:24 +1000

    Fix incorrect mod bus for Forge events

--- forge/src/main/java/software/bernie/geckolib/GeckoLibClient.java
+++ forge/src/main/java/software/bernie/geckolib/GeckoLibClient.java
@@ -6,7 +6,7 @@ import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import software.bernie.geckolib.cache.GeckoLibResources;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = GeckoLibConstants.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = GeckoLibConstants.MODID)
 public class GeckoLibClient {
     @SubscribeEvent
     public static void registerReloadListeners(final RegisterClientReloadListenersEvent ev) {
