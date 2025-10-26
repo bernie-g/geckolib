6bbd7fee644f023d2b3d157d77ba8990c7a996b5
Alex <95159836+DarksideAlex@users.noreply.github.com>
2025-10-26 10:07:52 +0100

    Update FileLoader.java

--- Fabric/src/main/java/software/bernie/geckolib/loading/FileLoader.java
+++ Fabric/src/main/java/software/bernie/geckolib/loading/FileLoader.java
@@ -19,47 +19,43 @@ import java.nio.charset.Charset;
  * Extracts raw information from given files, and other similar functions
  */
 public final class FileLoader {
-	/**
-	 * Load up and deserialize an animation json file to its respective {@link software.bernie.geckolib.core.animation.Animation} components
-	 * @param location The resource path of the animations file
-	 * @param manager The Minecraft {@code ResourceManager} responsible for maintaining in-memory resource access
-	 */
+
 	public static BakedAnimations loadAnimationsFile(ResourceLocation location, ResourceManager manager) {
-		return JsonUtil.GEO_GSON.fromJson(GsonHelper.getAsJsonObject(loadFile(location, manager), "animations"), BakedAnimations.class);
+		try {
+			JsonObject json = loadFile(location, manager);
+			return JsonUtil.GEO_GSON.fromJson(GsonHelper.getAsJsonObject(json, "animations"), BakedAnimations.class);
+		} catch (Exception e) {
+			GeckoLib.LOGGER.warn("Couldn't load " + location);
+			return new BakedAnimations();
+		}
 	}
 
-	/**
-	 * Load up and deserialize a geo model json file to its respective {@link BakedGeoModel} format
-	 *
-	 * @param location The resource path of the model file
-	 * @param manager The Minecraft {@code ResourceManager} responsible for maintaining in-memory resource access
-	 */
 	public static Model loadModelFile(ResourceLocation location, ResourceManager manager) {
-		return JsonUtil.GEO_GSON.fromJson(loadFile(location, manager), Model.class);
+		try {
+			JsonObject json = loadFile(location, manager);
+			return JsonUtil.GEO_GSON.fromJson(json, Model.class);
+		} catch (Exception e) {
+			GeckoLib.LOGGER.warn("Couldn't load " + location);
+			return new Model();
+		}
 	}
 
-	/**
-	 * Load a given json file into memory
-	 * @param location The resource path of the json file
-	 * @param manager The Minecraft {@code ResourceManager} responsible for maintaining in-memory resource access
-	 */
 	public static JsonObject loadFile(ResourceLocation location, ResourceManager manager) {
-		return GsonHelper.fromJson(JsonUtil.GEO_GSON, getFileContents(location, manager), JsonObject.class);
+		try {
+			String contents = getFileContents(location, manager);
+			return GsonHelper.fromJson(JsonUtil.GEO_GSON, contents, JsonObject.class);
+		} catch (Exception e) {
+			GeckoLib.LOGGER.warn("Couldn't load " + location);
+			return new JsonObject();
+		}
 	}
 
-	/**
-	 * Read a text-based file into memory in the form of a single string
-	 * @param location The resource path of the file
-	 * @param manager The Minecraft {@code ResourceManager} responsible for maintaining in-memory resource access
-	 */
 	public static String getFileContents(ResourceLocation location, ResourceManager manager) {
 		try (InputStream inputStream = manager.getResourceOrThrow(location).open()) {
 			return IOUtils.toString(inputStream, Charset.defaultCharset());
-		}
-		catch (Exception e) {
-			GeckoLib.LOGGER.error("Couldn't load " + location, e);
-
-			throw new RuntimeException(new FileNotFoundException(location.toString()));
+		} catch (Exception e) {
+			GeckoLib.LOGGER.warn("Couldn't load " + location);
+			return "";
 		}
 	}
 }
