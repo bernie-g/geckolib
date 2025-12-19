b8504dbedcba0c81d8e9e2a7cc4ca02baa91cc6d
Tslat <7098525+Tslat@users.noreply.github.com>
2025-12-19 23:27:21 +1000

    Clean up

--- common/src/main/java/software/bernie/geckolib/animatable/instance/InstancedAnimatableInstanceCache.java
+++ common/src/main/java/software/bernie/geckolib/animatable/instance/InstancedAnimatableInstanceCache.java
@@ -1,5 +1,7 @@
 package software.bernie.geckolib.animatable.instance;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
 
@@ -10,7 +12,7 @@ import software.bernie.geckolib.animatable.manager.AnimatableManager;
  * Use {@link software.bernie.geckolib.util.GeckoLibUtil#createInstanceCache GeckoLibUtil.createInstanceCache} instead
  */
 public class InstancedAnimatableInstanceCache extends AnimatableInstanceCache {
-	protected AnimatableManager<?> manager;
+	protected Supplier<AnimatableManager<?>> manager = Suppliers.memoize(() -> new AnimatableManager<>(this.animatable));
 
 	public InstancedAnimatableInstanceCache(GeoAnimatable animatable) {
 		super(animatable);
@@ -21,11 +23,9 @@ public class InstancedAnimatableInstanceCache extends AnimatableInstanceCache {
 	 * <p>
 	 * Because this cache subclass expects a 1:1 relationship of cache to animatable, only one {@code AnimatableManager} instance is used
 	 */
-	@Override
+	@SuppressWarnings("unchecked")
+    @Override
 	public AnimatableManager<?> getManagerForId(long uniqueId) {
-		if (this.manager == null)
-			this.manager = new AnimatableManager<>(this.animatable);
-
-		return this.manager;
+		return this.manager.get();
 	}
 }
