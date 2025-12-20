0c3d780e42d189cfe5b2083c27d1bbffe5f06897
Tslat <7098525+Tslat@users.noreply.github.com>
2025-12-20 10:46:20 +1000

    Add some documentation and logging

--- common/src/main/java/software/bernie/geckolib/loading/json/raw/ModelProperties.java
+++ common/src/main/java/software/bernie/geckolib/loading/json/raw/ModelProperties.java
@@ -6,15 +6,22 @@ import com.google.gson.JsonParseException;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.Nullable;
+import software.bernie.geckolib.GeckoLibConstants;
 import software.bernie.geckolib.util.JsonUtil;
 
 /**
  * Container class for model property information, only used in deserialization at startup
  *
+ * @param identifier The asset identifier for this model. Not used by GeckoLib
+ * @param visibleBoundsWidth The width of the visible bounds for this model. Not used by GeckoLib
+ * @param visibleBoundsHeight The height of the visible bounds for this model. Not used by GeckoLib
+ * @param visibleBoundsOffset The offset of the visible bounds for this model. Not used by GeckoLib
+ * @param textureWidth The width of the texture for this model. Technically optional, but GeckoLib requires it
+ * @param textureHeight The height of the texture for this model. Technically optional, but GeckoLib requires it
  * @see <a href="https://learn.microsoft.com/en-us/minecraft/creator/reference/content/schemasreference/schemas/minecraftschema_geometry_1.21.0?view=minecraft-bedrock-experimental">Bedrock Geometry Spec 1.21.0</a>
  */
 public record ModelProperties(String identifier, @Nullable Float visibleBoundsWidth, @Nullable Float visibleBoundsHeight, @Nullable Vec3 visibleBoundsOffset,
-							  @Nullable Integer textureWidth, @Nullable Integer textureHeight) {
+							  int textureWidth, int textureHeight) {
 	public static JsonDeserializer<ModelProperties> deserializer() throws JsonParseException {
 		return (json, type, context) -> {
 			final JsonObject obj = json.getAsJsonObject();
@@ -22,8 +29,11 @@ public record ModelProperties(String identifier, @Nullable Float visibleBoundsWi
 			final Float visibleBoundsWidth = JsonUtil.getOptionalFloat(obj, "visible_bounds_width");
 			final Float visibleBoundsHeight = JsonUtil.getOptionalFloat(obj, "visible_bounds_height");
 			final double[] visibleBoundsOffset = JsonUtil.jsonArrayToDoubleArray(GsonHelper.getAsJsonArray(obj, "visible_bounds_offset", null));
-			final int textureWidth = GsonHelper.getAsInt(obj, "texture_width");
-			final int textureHeight = GsonHelper.getAsInt(obj, "texture_height");
+			final int textureWidth = GsonHelper.getAsInt(obj, "texture_width", 16);
+			final int textureHeight = GsonHelper.getAsInt(obj, "texture_height", 16);
+
+			if (!obj.has("texture_width") || !obj.has("texture_height"))
+				GeckoLibConstants.LOGGER.warn("GeckoLib model {} does not have texture dimensions specified, likely an invalid geometry json!", identifier);
 
 			return new ModelProperties(identifier == null ? String.valueOf(obj.hashCode()) : identifier,
 									   visibleBoundsWidth, visibleBoundsHeight, new Vec3(visibleBoundsOffset[0], visibleBoundsOffset[1], visibleBoundsOffset[2]),
