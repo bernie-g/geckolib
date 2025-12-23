ddab40f36298084c17ac7e759c1f2eae4eb0bbe0
Tslat <still.beef1211@fastmail.com>
2025-12-23 10:19:32 +1000

    Add package info for geometry package

---
+++ common/src/main/java/software/bernie/geckolib/loading/definition/geometry/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Base .geo adapter classes
+ */
+@NullMarked
+@ApiStatus.Internal
+package software.bernie.geckolib.loading.definition.geometry;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
