1424ff75d7e263715ece62e23e4ed0053d627008
Tslat <still.beef1211@fastmail.com>
2025-12-24 15:33:45 +1000

    Add fallback loader and private LOADER field to prep for use

--- common/src/main/java/software/bernie/geckolib/GeckoLibServices.java
+++ common/src/main/java/software/bernie/geckolib/GeckoLibServices.java
@@ -1,11 +1,11 @@
 package software.bernie.geckolib;
 
-import software.bernie.geckolib.service.GeckoLibEvents;
-import software.bernie.geckolib.service.GeckoLibNetworking;
-import software.bernie.geckolib.service.GeckoLibPlatform;
-import software.bernie.geckolib.service.GeckoLibClient;
+import org.jspecify.annotations.Nullable;
+import software.bernie.geckolib.service.*;
 
+import java.util.Optional;
 import java.util.ServiceLoader;
+import java.util.function.Supplier;
 
 /**
  * Service holder class for access to the various SPI platforms that GeckoLib uses
@@ -13,6 +13,7 @@ import java.util.ServiceLoader;
 public final class GeckoLibServices {
     public static final GeckoLibPlatform PLATFORM = load(GeckoLibPlatform.class);
     public static final GeckoLibNetworking NETWORK = load(GeckoLibNetworking.class);
+    private static final GeckoLibLoader LOADER = load(GeckoLibLoader.class, null);
 
     public static class Client {
         public static final GeckoLibEvents EVENTS = load(GeckoLibEvents.class);
@@ -20,9 +21,15 @@ public final class GeckoLibServices {
     }
 
     private static <T> T load(Class<T> clazz) {
+        return load(clazz, null);
+    }
+
+    private static <T> T load(Class<T> clazz, @Nullable Supplier<T> defaultInstance) {
         final T loadedService = ServiceLoader.load(clazz)
                 .findFirst()
+                .or(defaultInstance == null ? Optional::empty : () -> Optional.of(defaultInstance.get()))
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+
         GeckoLibConstants.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
 
         return loadedService;
