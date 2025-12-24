e462c41b7f9b621657065fb3226ca0f9a9473fb0
Tslat <still.beef1211@fastmail.com>
2025-12-24 10:54:08 +1000

    Add currently unused SPI for future use

---
+++ common/src/main/java/software/bernie/geckolib/service/GeckoLibDeserialization.java
@@ -0,0 +1,36 @@
+package software.bernie.geckolib.service;
+
+import com.google.gson.Gson;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.resources.Resource;
+import software.bernie.geckolib.cache.animation.Animation;
+import software.bernie.geckolib.cache.model.BakedGeoModel;
+import software.bernie.geckolib.loading.definition.animation.ActorAnimations;
+import software.bernie.geckolib.loading.definition.geometry.Geometry;
+
+/**
+ * Replaceable SPI for alternate deserialization schemes, replacing GeckoLib's default {@link Gson} handling
+ * <p>
+ * Not currently used by GeckoLib
+ */
+public interface GeckoLibDeserialization {
+    /**
+     * Deserialize an identified resource into an unbaked {@link Geometry} instance, ready for
+     * transposition into a usable {@link BakedGeoModel}
+     *
+     * @param id The resource path of the resource to load
+     * @param resource The resource reference to load
+     * @return An unbaked Geometry instance
+     */
+    Geometry deserializeGeckoLibModelFile(Identifier id, Resource resource);
+
+    /**
+     * Deserialize an identified resource into an unbaked {@link ActorAnimations} instance, ready for
+     * transposition into a usable {@link Animation}
+     *
+     * @param id The resource path of the resource to load
+     * @param resource The resource reference to load
+     * @return An unbaked ActorAnimations instance
+     */
+    ActorAnimations deserializeGeckoLibAnimationFile(Identifier id, Resource resource);
+}
