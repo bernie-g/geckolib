b3bcf39cfbb96234b1547e49683d8baee58d0651
Tslat <7098525+Tslat@users.noreply.github.com>
2026-02-14 09:40:51 +1000

    Rename variable to better reflect value

--- common/src/main/java/software/bernie/geckolib/loading/loader/GeckoLibGsonLoader.java
+++ common/src/main/java/software/bernie/geckolib/loading/loader/GeckoLibGsonLoader.java
@@ -8,10 +8,9 @@ import software.bernie.geckolib.GeckoLibConstants;
 import software.bernie.geckolib.cache.model.BakedGeoModel;
 import software.bernie.geckolib.loading.definition.animation.ActorAnimations;
 import software.bernie.geckolib.loading.definition.geometry.Geometry;
-import software.bernie.geckolib.loading.json.ModelFormatVersion;
+import software.bernie.geckolib.loading.definition.geometry.object.ModelFormatVersion;
 import software.bernie.geckolib.loading.object.BakedAnimations;
 import software.bernie.geckolib.object.CompoundException;
-import software.bernie.geckolib.service.GeckoLibLoader;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -20,85 +19,85 @@ import java.io.Reader;
 public class GeckoLibGsonLoader implements GeckoLibLoader<JsonObject> {
     /// Read a GeckoLib model resource from disk into memory, deserializing it into type `T`
     ///
-    /// @param id The resource path of the resource to load
+    /// @param resourcePath The resource path of the resource to load
     /// @param resource The resource reference to load
     /// @return An instance of type `T` ready for baking
     @Override
-    public JsonObject deserializeGeckoLibModelFile(Identifier id, Resource resource) throws RuntimeException {
-        return readResourceAsJson(id, resource);
+    public JsonObject deserializeGeckoLibModelFile(Identifier resourcePath, Resource resource) throws RuntimeException {
+        return readResourceAsJson(resourcePath, resource);
     }
 
     /// Read a GeckoLib animations resource from disk into memory, deserializing it into type `T`
     ///
-    /// @param id The resource path of the resource to load
+    /// @param resourcePath The resource path of the resource to load
     /// @param resource The resource reference to load
     /// @return An instance of type `T` ready for baking
     @Override
-    public JsonObject deserializeGeckoLibAnimationFile(Identifier id, Resource resource) throws RuntimeException {
-        return readResourceAsJson(id, resource);
+    public JsonObject deserializeGeckoLibAnimationFile(Identifier resourcePath, Resource resource) throws RuntimeException {
+        return readResourceAsJson(resourcePath, resource);
     }
 
     /// Bake a GeckoLib model file into its final [BakedGeoModel] form from the raw `T` type instance
     /// read into memory in [#deserializeGeckoLibModelFile]
     ///
-    /// @param id The resource path of the animations file that was loaded
+    /// @param resourcePath The resource path of the animations file that was loaded
     /// @param raw The raw `T` type object read in [#deserializeGeckoLibModelFile]
     @Override
-    public BakedGeoModel bakeGeckoLibModelFile(Identifier id, JsonObject raw) throws RuntimeException {
-        return bakeJsonModel(id, raw).bake(id);
+    public BakedGeoModel bakeGeckoLibModelFile(Identifier resourcePath, JsonObject raw) throws RuntimeException {
+        return bakeJsonModel(resourcePath, raw).bake(resourcePath);
     }
 
     /// Bake a GeckoLib animations file into its final [BakedAnimations] form from the raw `T` type instance
     /// read into memory in [#deserializeGeckoLibAnimationFile]
     ///
-    /// @param id The resource path of the model file that was loaded
+    /// @param resourcePath The resource path of the model file that was loaded
     /// @param raw The raw `T` type object read in [#deserializeGeckoLibAnimationFile]
     @Override
-    public BakedAnimations bakeGeckoLibAnimationsFile(Identifier id, JsonObject raw) throws RuntimeException {
-        return bakeJsonAnimations(id, raw).bake(id);
+    public BakedAnimations bakeGeckoLibAnimationsFile(Identifier resourcePath, JsonObject raw) throws RuntimeException {
+        return bakeJsonAnimations(resourcePath, raw).bake(resourcePath);
     }
 
     /// Read a single resource into its [JsonObject] form
-    protected static JsonObject readResourceAsJson(Identifier id, Resource resource) throws RuntimeException {
+    protected static JsonObject readResourceAsJson(Identifier resourcePath, Resource resource) throws RuntimeException {
         try (Reader reader = resource.openAsReader()) {
             return GsonHelper.parse(reader);
         }
         catch (IOException ex) {
-            throw GeckoLibConstants.exception(id, "Error reading JSON file", ex);
+            throw GeckoLibConstants.exception(resourcePath, "Error reading JSON file", ex);
         }
     }
 
     /// Bake a [Geometry] from its [JsonObject] serialized form
-    protected static Geometry bakeJsonModel(Identifier path, JsonObject json) throws RuntimeException {
-        if (path.getPath().endsWith(".animation.json"))
-            throw new IllegalStateException("Found animation file found in models folder! '" + path + "'");
+    protected static Geometry bakeJsonModel(Identifier resourcePath, JsonObject json) throws RuntimeException {
+        if (resourcePath.getPath().endsWith(".animation.json"))
+            throw new IllegalStateException("Found animation file found in models folder! '" + resourcePath + "'");
 
         final Geometry geometry = Geometry.GSON.fromJson(json, Geometry.class);
         final ModelFormatVersion matchedVersion = ModelFormatVersion.match(geometry.formatVersion());
 
         if (matchedVersion == null) {
-            GeckoLibConstants.LOGGER.warn("{}: Unknown geo model format version: '{}'. This may not work correctly", path, geometry.formatVersion());
+            GeckoLibConstants.LOGGER.warn("{}: Unknown geo model format version: '{}'. This may not work correctly", resourcePath, geometry.formatVersion());
         }
         else if (!matchedVersion.isSupported()) {
-            GeckoLibConstants.LOGGER.error("{}: Unsupported geo model format version: '{}'. {}", path, geometry.formatVersion(), matchedVersion.getErrorMessage());
+            GeckoLibConstants.LOGGER.error("{}: Unsupported geo model format version: '{}'. {}", resourcePath, geometry.formatVersion(), matchedVersion.getErrorMessage());
         }
 
         return geometry;
     }
 
     /// Bake the [ActorAnimations] from a [JsonObject] serialized form
-    protected static ActorAnimations bakeJsonAnimations(Identifier path, JsonObject json) throws RuntimeException {
-        if (path.getPath().endsWith(".geo.json"))
-            throw new IllegalStateException("Found model file in animations folder! '" + path + "'");
+    protected static ActorAnimations bakeJsonAnimations(Identifier resourcePath, JsonObject json) throws RuntimeException {
+        if (resourcePath.getPath().endsWith(".geo.json"))
+            throw new IllegalStateException("Found model file in animations folder! '" + resourcePath + "'");
 
         try {
             return ActorAnimations.GSON.fromJson(json, ActorAnimations.class);
         }
         catch (CompoundException ex) {
-            throw ex.withMessage(path + ": Error building animations from JSON");
+            throw ex.withMessage(resourcePath + ": Error building animations from JSON");
         }
         catch (Exception ex) {
-            throw GeckoLibConstants.exception(path, "Error building animations from JSON", ex);
+            throw GeckoLibConstants.exception(resourcePath, "Error building animations from JSON", ex);
         }
     }
 }
