779aa1d2d84e70760856d74ae2b7abcec4c99322
Tslat <7098525+Tslat@users.noreply.github.com>
2026-03-03 21:30:34 +1000

    Fixed MathParser not parsing group negatives properly

--- common/src/main/java/software/bernie/geckolib/loading/math/MathParser.java
+++ common/src/main/java/software/bernie/geckolib/loading/math/MathParser.java
@@ -335,8 +335,16 @@ public class MathParser {
                     }
 
                     if (groupState == 0) {
-                        if (!buffer.isEmpty())
-                            subValues.add(parseSymbols(compileSymbols(buffer.toString().toCharArray())));
+                        if (!buffer.isEmpty()) {
+                            if (!symbols.isEmpty() && symbols.getLast().left().filter("-"::equals).isPresent() &&
+                                (symbols.size() == 1 || symbols.get(symbols.size() - 2).left().filter(Operator::isOperator).isPresent())) {
+                                symbols.removeLast();
+                                subValues.add(new Negative(parseSymbols(compileSymbols(buffer.toString().toCharArray()))));
+                            }
+                            else {
+                                subValues.add(parseSymbols(compileSymbols(buffer.toString().toCharArray())));
+                            }
+                        }
 
                         i = j;
 
@@ -391,7 +399,7 @@ public class MathParser {
         if (symbols.size() == 1)
             return compileSingleValue(symbols.getFirst());
 
-        Optional<Ternary> ternary = compileTernary(symbols);
+        final Optional<Ternary> ternary = compileTernary(symbols);
 
         if (ternary.isPresent())
             return ternary;
