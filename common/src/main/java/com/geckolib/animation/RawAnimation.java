f1d32b2c1f515fb6bf4124e41226b93ca5518ee6
Tslat <7098525+Tslat@users.noreply.github.com>
2026-08-29 18:57:11 +1000

    Fix Stage comparison equality

--- common/src/main/java/com/geckolib/animation/RawAnimation.java
+++ common/src/main/java/com/geckolib/animation/RawAnimation.java
@@ -173,10 +173,12 @@ public final class RawAnimation {
 			if (this == obj)
 				return true;
 
-			if (obj == null || getClass() != obj.getClass())
+			if (!(obj instanceof Stage(String name, LoopType type, int ticks)))
 				return false;
 
-			return hashCode() == obj.hashCode();
+			return name.equals(this.animationName) &&
+                   type == this.loopType &&
+                   ticks == this.waitTicks;
 		}
 
 		@Override
