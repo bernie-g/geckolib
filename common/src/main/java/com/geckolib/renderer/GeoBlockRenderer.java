60f38397abb8156fff0315dae0e30f1340937f3a
Tslat <7098525+Tslat@users.noreply.github.com>
2026-08-29 18:52:50 +1000

    Various render performance improvements

--- common/src/main/java/com/geckolib/renderer/GeoBlockRenderer.java
+++ common/src/main/java/com/geckolib/renderer/GeoBlockRenderer.java
@@ -44,7 +44,7 @@ import java.util.function.Function;
 /// @param <R> RenderState class type
 public class GeoBlockRenderer<T extends BlockEntity & GeoAnimatable, R extends BlockEntityRenderState> implements GeoRenderer<T, Void, R>, BlockEntityRenderer<T, R> {
     @SuppressWarnings("unchecked")
-    private static final EnumProperty<Direction>[] SUPPORTED_DIRECTION_PROPERTIES = new EnumProperty[] {BlockStateProperties.FACING, BlockStateProperties.HORIZONTAL_FACING, BlockStateProperties.VERTICAL_DIRECTION, BlockStateProperties.FACING_HOPPER };
+    protected static final EnumProperty<Direction>[] SUPPORTED_DIRECTION_PROPERTIES = new EnumProperty[] {BlockStateProperties.FACING, BlockStateProperties.HORIZONTAL_FACING, BlockStateProperties.VERTICAL_DIRECTION, BlockStateProperties.FACING_HOPPER };
     public static final DataTicket<Direction> DIRECTION_FACING = DataTicket.create("geoblockrenderer_direction_facing", new TypeToken<>() {});
 	protected final GeoRenderLayersContainer<T, Void, R> renderLayers = new GeoRenderLayersContainer<>(this);
 	protected final GeoModel<T> model;
