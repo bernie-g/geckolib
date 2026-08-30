573c469ff07e96544da563534ae5a5503e08af12
Tslat <7098525+Tslat@users.noreply.github.com>
2026-08-30 11:05:25 +1000

    Fix higher-level operator precedence

--- common/src/main/java/com/geckolib/loading/math/Operator.java
+++ common/src/main/java/com/geckolib/loading/math/Operator.java
@@ -19,23 +19,23 @@ public record Operator(String symbol, int precedence, Operation operation) imple
     private static final CharSet OPERATOR_SYMBOLS = Util.make(new CharOpenHashSet(15), set -> set.addAll(Arrays.asList('?', ':', ',')));
     private static int LONGEST_OPERATOR;
 
-    public static final Operator ADD = register("+", 1, Double::sum);
-    public static final Operator SUB = register("-", 1, (a, b) -> a - b);
-    public static final Operator MUL = register("*", 2, (a, b) -> a * b);
-    public static final Operator DIV = register("/", 2, (a, b) -> b == 0 ? a : a / b);
-    public static final Operator MOD = register("%", 2, (a, b) -> b == 0 ? a : a % b);
-    public static final Operator POW = register("^", 3, Math::pow);
-    public static final Operator AND = registerAlias(register("&&", 5, (a, b) -> a != 0 && b != 0 ? 1 : 0),
-                                                     "&");
-    public static final Operator OR = registerAlias(register("||", 5, (a, b) -> a != 0 || b != 0 ? 1 : 0),
+    public static final Operator ASSIGN_VARIABLE = register("=", 1, (a, b) -> 0);
+    public static final Operator OR = registerAlias(register("||", 2, (a, b) -> a != 0 || b != 0 ? 1 : 0),
                                                     "|");
+    public static final Operator AND = registerAlias(register("&&", 3, (a, b) -> a != 0 && b != 0 ? 1 : 0),
+                                                     "&");
+    public static final Operator EQUAL = register("==", 4, (a, b) -> Math.abs(a - b) < 0.00001 ? 1 : 0);
+    public static final Operator NOT_EQUAL = register("!=", 4, (a, b) -> Math.abs(a - b) >= 0.00001 ? 1 : 0);
     public static final Operator LT = register("<", 5, (a, b) -> a < b ? 1 : 0);
     public static final Operator LTE = register("<=", 5, (a, b) -> a <= b ? 1 : 0);
     public static final Operator GT = register(">", 5, (a, b) -> a > b ? 1 : 0);
     public static final Operator GTE = register(">=", 5, (a, b) -> a >= b ? 1 : 0);
-    public static final Operator EQUAL = register("==", 5, (a, b) -> Math.abs(a - b) < 0.00001 ? 1 : 0);
-    public static final Operator NOT_EQUAL = register("!=", 5, (a, b) -> Math.abs(a - b) >= 0.00001 ? 1 : 0);
-    public static final Operator ASSIGN_VARIABLE = register("=", Integer.MAX_VALUE, (a, b) -> 0);
+    public static final Operator ADD = register("+", 6, Double::sum);
+    public static final Operator SUB = register("-", 6, (a, b) -> a - b);
+    public static final Operator MUL = register("*", 7, (a, b) -> a * b);
+    public static final Operator DIV = register("/", 7, (a, b) -> b == 0 ? a : a / b);
+    public static final Operator MOD = register("%", 7, (a, b) -> b == 0 ? a : a % b);
+    public static final Operator POW = register("^", 8, Math::pow);
 
     /// Instantiate and register a new mathematical operator.
     /// Note that it should be a functionally distinct operator from other existing operators.
