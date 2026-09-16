998d1af0bab86e0a1d877a2a8e30b246be539d3c
Tslat <7098525+Tslat@users.noreply.github.com>
2026-09-16 16:21:21 +1000

    Add operator aliases and fix operator precedence

--- common/src/main/java/software/bernie/geckolib/loading/math/Operator.java
+++ common/src/main/java/software/bernie/geckolib/loading/math/Operator.java
@@ -22,21 +22,23 @@ public record Operator(String symbol, int precedence, Operation operation) imple
     private static final CharSet OPERATOR_SYMBOLS = Util.make(new CharOpenHashSet(15), set -> set.addAll(Arrays.asList('?', ':', ',')));
     private static int LONGEST_OPERATOR;
 
-    public static final Operator ADD = register("+", 1, (a, b) -> a + b);
-    public static final Operator SUB = register("-", 1, (a, b) -> a - b);
-    public static final Operator MUL = register("*", 2, (a, b) -> a * b);
-    public static final Operator DIV = register("/", 2, (a, b) -> b == 0 ? a : a / b);
-    public static final Operator MOD = register("%", 2, (a, b) -> b == 0 ? a : a % b);
-    public static final Operator POW = register("^", 3, Math::pow);
-    public static final Operator AND = register("&&", 5, (a, b) -> a != 0 && b != 0 ? 1 : 0);
-    public static final Operator OR = register("||", 5, (a, b) -> a != 0 || b != 0 ? 1 : 0);
+    public static final Operator ASSIGN_VARIABLE = register("=", 1, (a, b) -> 0);
+    public static final Operator OR = registerAlias(register("||", 2, (a, b) -> a != 0 || b != 0 ? 1 : 0),
+                                                    "|");
+    public static final Operator AND = registerAlias(register("&&", 3, (a, b) -> a != 0 && b != 0 ? 1 : 0),
+                                                     "&");
+    public static final Operator EQUAL = register("==", 4, (a, b) -> Math.abs(a - b) < 0.00001 ? 1 : 0);
+    public static final Operator NOT_EQUAL = register("!=", 4, (a, b) -> Math.abs(a - b) >= 0.00001 ? 1 : 0);
     public static final Operator LT = register("<", 5, (a, b) -> a < b ? 1 : 0);
     public static final Operator LTE = register("<=", 5, (a, b) -> a <= b ? 1 : 0);
     public static final Operator GT = register(">", 5, (a, b) -> a > b ? 1 : 0);
     public static final Operator GTE = register(">=", 5, (a, b) -> a >= b ? 1 : 0);
-    public static final Operator EQUAL = register("==", 5, (a, b) -> Math.abs(a - b) < 0.00001 ? 1 : 0);
-    public static final Operator NOT_EQUAL = register("!=", 5, (a, b) -> Math.abs(a - b) >= 0.00001 ? 1 : 0);
-    public static final Operator ASSIGN_VARIABLE = register("=", Integer.MAX_VALUE, (a, b) -> 0);
+    public static final Operator ADD = register("+", 6, Double::sum);
+    public static final Operator SUB = register("-", 6, (a, b) -> a - b);
+    public static final Operator MUL = register("*", 7, (a, b) -> a * b);
+    public static final Operator DIV = register("/", 7, (a, b) -> b == 0 ? a : a / b);
+    public static final Operator MOD = register("%", 7, (a, b) -> b == 0 ? a : a % b);
+    public static final Operator POW = register("^", 8, Math::pow);
 
     /**
      * Instantiate and register a new mathematical operator.
@@ -59,6 +61,28 @@ public record Operator(String symbol, int precedence, Operation operation) imple
         LONGEST_OPERATOR = Math.max(LONGEST_OPERATOR, symbol.length());
 
         return operator;
+    }
+
+    /**
+     * Register one or more alias operator symbols for an existing operator
+     *
+     * @param original The target operator these aliases are for
+     * @param aliases The list of alias symbols for this operator
+     * @return The target operator
+     */
+    public static Operator registerAlias(Operator original, String... aliases) {
+        for (String alias : aliases) {
+            if (OPERATORS.put(alias, original) != null)
+                throw new IllegalArgumentException("Attempting to register an already existing operator! '" + alias + "'");
+
+            for (char symbolChar : alias.toCharArray()) {
+                OPERATOR_SYMBOLS.add(symbolChar);
+            }
+
+            LONGEST_OPERATOR = Math.max(LONGEST_OPERATOR, alias.length());
+        }
+
+        return original;
     }
 
     /**
